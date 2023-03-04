c33616b9bd15abd926eb6cf74cc2d30b53e9926b
오성혁 <68771488+ohsung1125@users.noreply.github.com>
2023-03-05 02:37:19 +0900

    Revert "feat: set page routing and add basic pages"

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,16 +1,15 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
