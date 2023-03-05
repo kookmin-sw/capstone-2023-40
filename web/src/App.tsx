ef30f3ebe24d2ebc75d2c17f32fef4827141a57b
sanghyun1128 <sanghyun1128@gmail.com>
2023-03-05 18:07:56 +0900

    feat: set page routing

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import MainPage from './routes/MainPage';
+import LoginPage from './routes/LoginPage';
+import RegisterPage from './routes/RegisterPage';
+import SurveyListPage from './routes/SurveyPages/SurveyListPage';
+import SurveyPage from './routes/SurveyPages/SurveyPage';
+import SurveyFormPage from './routes/SurveyPages/SurveyFormPage';
+import SurveyLoginReqPage from './routes/SurveyPages/SurveyLoginReqPage';
+import ProfilePage from './routes/MyPages/ProfilePage';
+import AuthListPage from './routes/MyPages/AuthListPage';
+import SurveyResPage from './routes/MyPages/SurveyResPage';
+import NotFound from './routes/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/survey" element={<SurveyListPage />} />
+        <Route path="/survey/:id" element={<SurveyPage />} />
+        <Route path="/survey/form" element={<SurveyFormPage />} />
+        <Route path="/survey/login-required" element={<SurveyLoginReqPage />} />
+        <Route path="/mypage" element={<ProfilePage />} />
+        <Route path="/mypage/auth-list" element={<AuthListPage />} />
+        <Route path="/mypage/survey-result/:id" element={<SurveyResPage />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
