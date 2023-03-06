d9f474d187bf2292ae19e4245b2be91b601cd123
SeiwonPark <psw7347@gmail.com>
2023-03-06 16:15:34 +0900

    feat: add light & dark theme

---
+++ web/src/themes/darkTheme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from 'styled-components';
+
+export const darkTheme: DefaultTheme = {
+  borderRadius: '4vh',
+
+  colors: {
+    default: '#FFFFFF',
+    primary: '#FF8A00',
+    text: '#EBEFF7',
+    button: '#7B87A0',
+    background: '#202123',
+  },
+};
---
+++ web/src/themes/lightTheme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from 'styled-components';
+
+export const lightTheme: DefaultTheme = {
+  borderRadius: '4vh',
+
+  colors: {
+    default: '#000000',
+    primary: '#FF8A00',
+    text: '#7B87A0',
+    button: '#EBEFF7',
+    background: '#F8F8F8',
+  },
+};
