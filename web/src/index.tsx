223436ab3eb68c0f2d44565fa13e025b28f4822f
SeiwonPark <psw7347@gmail.com>
2023-03-06 20:54:14 +0900

    chore: formated imports

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,7 +1,8 @@
-import ReactDOM from 'react-dom/client';
-import './index.css';
 import React from 'react';
+
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
--- web/src/routes/SurveyPages/SurveyPage.tsx
+++ web/src/routes/SurveyPages/SurveyPage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { useParams } from 'react-router-dom';
 
 export default function SurveyPage() {
