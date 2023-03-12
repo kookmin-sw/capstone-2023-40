5908a39c69c20cca2d1de7ff06d9d745ac55c412
sanghyun1128 <sanghyun1128@gmail.com>
2023-03-12 22:07:05 +0900

    feat: make to get survey lists from api server

--- web/src/routes/SurveyPages/SurveyListPage.tsx
+++ web/src/routes/SurveyPages/SurveyListPage.tsx
@@ -1,5 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import styled from 'styled-components';
+
+import axios from '../../api/axios';
+import requests from '../../api/request';
+import Header from '../../components/Header';
+import { useTheme } from '../../hooks/useTheme';
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5));
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+`;
 
 export default function SurveyListPage() {
-  return <div>SurveyListPage</div>;
+  const [theme, toggleTheme] = useTheme();
+  const [surveys, setSurveys] = useState([]);
+
+  const fetchSurveyData = async () => {
+    const request = await axios.get(requests.fetchSurveyList);
+    setSurveys(request.data);
+    return request;
+  };
+
+  useEffect(() => {
+    fetchSurveyData();
+  }, []);
+
+  return (
+    <Container>
+      <Header theme={theme} toggleTheme={toggleTheme} />
+    </Container>
+  );
 }
