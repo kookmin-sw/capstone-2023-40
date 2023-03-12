ba6d60d47685b03081ab643b426bf50c6455bfcf
sanghyun1128 <sanghyun1128@gmail.com>
2023-03-13 00:17:53 +0900

    feat: make survey list table

--- web/src/routes/SurveyPages/SurveyListPage.tsx
+++ web/src/routes/SurveyPages/SurveyListPage.tsx
@@ -11,15 +11,20 @@ import { useTheme } from '../../hooks/useTheme';
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
-  background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5));
+  background-color: ${(props) => props.theme.colors.container};
   -webkit-background-size: cover;
   -moz-background-size: cover;
   -o-background-size: cover;
   background-size: cover;
 `;
 
+const SurveyListContainer = styled.table``;
+const SurveyListHead = styled.thead``;
+const SurveyListBody = styled.tbody``;
+const SurveyColumn = styled.tr``;
+
 interface Survey {
-  author_id: number;
+  author_id: string;
   title: string;
   created_at: string;
   started_at: string;
@@ -43,15 +48,27 @@ export default function SurveyListPage() {
   }, []);
 
   return (
-    <Container>
+    <Container theme={theme}>
       <Header theme={theme} toggleTheme={toggleTheme} />
-      <ul>
-        {surveys.map((survey) => (
-          <li key={survey.survey_id}>
-            {survey.title} ({survey.must_auth_list})
-          </li>
-        ))}
-      </ul>
+      <SurveyListContainer theme={theme}>
+        <SurveyListHead theme={theme}>
+          <SurveyColumn>
+            <th>설문제목</th>
+            <th>필수인증</th>
+            <th>설문종료일</th>
+          </SurveyColumn>
+        </SurveyListHead>
+
+        <SurveyListBody>
+          {surveys.map((survey) => (
+            <SurveyColumn key={survey.survey_id} theme={theme}>
+              <td>{survey.title}</td>
+              <td>{survey.must_auth_list}</td>
+              <td>{survey.ended_at}</td>
+            </SurveyColumn>
+          ))}
+        </SurveyListBody>
+      </SurveyListContainer>
     </Container>
   );
 }
