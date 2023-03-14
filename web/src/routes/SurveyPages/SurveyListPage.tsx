e32f2f9358b0cd38dc80462b109c658473c49116
sanghyun1128 <sanghyun1128@gmail.com>
2023-03-14 18:50:57 +0900

    feat: css styling

--- web/src/routes/SurveyPages/SurveyListPage.tsx
+++ web/src/routes/SurveyPages/SurveyListPage.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 import { AxiosResponse } from 'axios';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 import axios from '../../api/axios';
@@ -11,17 +12,76 @@ import { useTheme } from '../../hooks/useTheme';
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
-  background-color: ${(props) => props.theme.colors.container};
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
+  ${(props) => props.theme.colors.container};
 `;
 
-const SurveyListContainer = styled.table``;
-const SurveyListHead = styled.thead``;
-const SurveyListBody = styled.tbody``;
-const SurveyColumn = styled.tr``;
+const ListContainer = styled.div`
+  width: 90vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  padding: 5vw;
+  background-color: ${(props) => props.theme.colors.container};
+`;
+
+const ListColumn = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+const Item = styled.div`
+  margin: 2px;
+  padding: 19px;
+  font-size: 17px;
+  font-weight: bold;
+  border-radius: 5px;
+  color: ${(props) => props.theme.colors.default};
+  background-color: ${(props) => props.theme.colors.header};
+`;
+
+const HeadItem = styled.div`
+  margin: 2px;
+  padding: 19px;
+  font-size: 17px;
+  font-weight: bolder;
+  text-align: center;
+  color: ${(props) => props.theme.colors.default};
+`;
+
+const Title = styled(Item)`
+  min-width: 15vh;
+  flex: 1;
+  &:hover {
+    background-color: ${(props) => props.theme.colors.btnhover};
+  }
+`;
+
+const Auth = styled(Item)`
+  min-width: 70px;
+  width: 17vw;
+`;
+
+const EndDate = styled(Item)`
+  min-width: 70px;
+  width: 13vw;
+  text-align: center;
+`;
+
+const HeadTitle = styled(HeadItem)`
+  min-width: 15vh;
+  flex: 1;
+`;
+
+const HeadAuth = styled(HeadItem)`
+  min-width: 70px;
+  width: 17vw;
+`;
+
+const HeadEndDate = styled(HeadItem)`
+  min-width: 70px;
+  width: 13vw;
+`;
 
 interface Survey {
   author_id: string;
@@ -36,6 +96,7 @@ interface Survey {
 export default function SurveyListPage() {
   const [theme, toggleTheme] = useTheme();
   const [surveys, setSurveys] = useState<Survey[]>([]);
+  const navigate = useNavigate();
 
   const fetchSurveyData = async (): Promise<AxiosResponse<Survey[]>> => {
     const request: AxiosResponse<Survey[]> = await axios.get<Survey[]>(requests.fetchSurveyList);
@@ -50,25 +111,23 @@ export default function SurveyListPage() {
   return (
     <Container theme={theme}>
       <Header theme={theme} toggleTheme={toggleTheme} />
-      <SurveyListContainer theme={theme}>
-        <SurveyListHead theme={theme}>
-          <SurveyColumn>
-            <th>설문제목</th>
-            <th>필수인증</th>
-            <th>설문종료일</th>
-          </SurveyColumn>
-        </SurveyListHead>
+      <ListContainer theme={theme}>
+        <ListColumn>
+          <HeadTitle theme={theme}>설문 제목</HeadTitle>
+          <HeadAuth theme={theme}>필수인증</HeadAuth>
+          <HeadEndDate theme={theme}>설문 종료일</HeadEndDate>
+        </ListColumn>
 
-        <SurveyListBody>
-          {surveys.map((survey) => (
-            <SurveyColumn key={survey.survey_id} theme={theme}>
-              <td>{survey.title}</td>
-              <td>{survey.must_auth_list}</td>
-              <td>{survey.ended_at}</td>
-            </SurveyColumn>
-          ))}
-        </SurveyListBody>
-      </SurveyListContainer>
+        {surveys.map((survey) => (
+          <ListColumn key={survey.survey_id} theme={theme}>
+            <Title onClick={() => navigate(`/survey/${survey.survey_id}`)} theme={theme}>
+              {survey.title}
+            </Title>
+            <Auth theme={theme}>{survey.must_auth_list}</Auth>
+            <EndDate theme={theme}>{survey.ended_at}</EndDate>
+          </ListColumn>
+        ))}
+      </ListContainer>
     </Container>
   );
 }
