0c9095a78bb222c6096faecf917c9594c72a87bb
sanghyun1128 <sanghyun1128@gmail.com>
2023-03-16 02:48:35 +0900

    fix: typo

--- web/src/api/request.ts
+++ web/src/api/request.ts
@@ -1,11 +1,11 @@
 interface ApiRequests {
-  healtCheck: string;
+  healthCheck: string;
   fetchSurveyListAll: string;
   fetchSurvey: string;
   fetchSurveyListPage: string;
 }
 const requests: ApiRequests = {
-  healtCheck: '/',
+  healthCheck: '/',
   fetchSurveyListAll: '/api/survey/all',
   fetchSurvey: '/api/survey/',
   fetchSurveyListPage: '/api/survey?page=',
