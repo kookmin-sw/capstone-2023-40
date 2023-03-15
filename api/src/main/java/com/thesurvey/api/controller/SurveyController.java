4993d90d35f0b99127442f0efd56312500eaf5b8
JinMyeong-Kim-Kr <kjmdkdlel@gmail.com>
2023-03-15 14:36:19 +0900

    feat: Implemented getSurveyWithId function in SurveyController.java

--- api/src/main/java/com/thesurvey/api/controller/SurveyController.java
+++ api/src/main/java/com/thesurvey/api/controller/SurveyController.java
@@ -1,19 +1,15 @@
 package com.thesurvey.api.controller;
 
 import com.thesurvey.api.domain.Survey;
-import com.thesurvey.api.domain.User;
-import com.thesurvey.api.dto.SurveyDto;
-import com.thesurvey.api.dto.UserDto;
 import com.thesurvey.api.service.SurveyService;
 import java.util.List;
-import org.apache.coyote.Request;
+import java.util.Optional;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -31,10 +27,11 @@ public class SurveyController {
         List<Survey> surveyList = surveyService.getAllSurvey();
         return ResponseEntity.ok(surveyList);
     }
-//    @GetMapping("/{id}")
-//    public ResponseEntity<List<Survey>> getSurveyWithId(@PathVariable Long surveyId) {
-//        return;
-//    }
+    @GetMapping("/{surveyId}")
+    public ResponseEntity<Optional<Survey>> getSurveyWithId(@PathVariable Long surveyId) {
+        Optional<Survey> survey = surveyService.getSurveyById(surveyId);
+        return ResponseEntity.ok(survey);
+    }
 
     @PostMapping
     public ResponseEntity<Survey> createSurvey(@RequestBody Survey survey) {
