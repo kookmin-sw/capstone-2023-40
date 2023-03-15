277054ea4d43439e95d93889ab5ab34d867c39fe
JinMyeong-Kim-Kr <kjmdkdlel@gmail.com>
2023-03-15 14:35:17 +0900

    Test: Added getSurveyWithId function for test in SurveyServiceTest

--- api/src/test/java/com/thesurvey/api/service/SurveyServiceTest.java
+++ api/src/test/java/com/thesurvey/api/service/SurveyServiceTest.java
@@ -2,6 +2,7 @@ package com.thesurvey.api.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.util.AssertionErrors.assertTrue;
 import com.thesurvey.api.domain.Question;
 import com.thesurvey.api.domain.Survey;
 import com.thesurvey.api.dto.SurveyDto;
@@ -27,7 +28,7 @@ public class SurveyServiceTest {
 
     @Test
     void createSurvey() {
-        String title = "title";
+        String title = "My name is Jin";
         Question question1 = new Question("what's your name?");
         Question question2 = new Question("what's your id?");
         List<Question> questions = Arrays.asList(question1, question2);
@@ -40,6 +41,26 @@ public class SurveyServiceTest {
         assertNotNull(savedSurvey);
         assertEquals(title, savedSurvey.get().getTitle());
         assertEquals(questions.size(), savedSurvey.get().getQuestions().size());
+
+
+    }
+
+    @Test
+    void getSurveyById() {
+        String title = "My name is Jin";
+        Question question1 = new Question("what's your name?");
+        Question question2 = new Question("what's your id?");
+        List<Question> questions = Arrays.asList(question1, question2);
+        Survey survey = new Survey(title, questions);
+        Survey newSurvey = surveyService.createSurvey(survey);
+        questionService.addQuestion(survey.getQuestions());
+
+        Optional<Survey> targetSurvey = surveyService.getSurveyById(newSurvey.getSurveyId());
+
+        assertEquals(survey.getSurveyId(), targetSurvey.get().getSurveyId());
+        assertEquals(survey.getTitle(), targetSurvey.get().getTitle());
+        assertEquals(survey.getQuestions().get(1).getContent(),
+            targetSurvey.get().getQuestions().get(1).getContent());
 
 
     }
