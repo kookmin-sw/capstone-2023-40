247bb85268df0b2abfb68cb959f46633ee550115
SeiwonPark <psw7347@gmail.com>
2023-03-20 00:22:21 +0900

    feat: add LocalDateTime for survey

--- api/src/main/java/com/thesurvey/api/domain/Survey.java
+++ api/src/main/java/com/thesurvey/api/domain/Survey.java
@@ -21,7 +21,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "survey")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Survey {
+public class Survey extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
