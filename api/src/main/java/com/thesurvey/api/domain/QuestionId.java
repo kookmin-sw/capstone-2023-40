b56285e96126c35128cf5faa0dfcf80e087e5707
SeiwonPark <psw7347@gmail.com>
2023-03-21 01:52:55 +0900

    refactor: group composite keys for question entity

---
+++ api/src/main/java/com/thesurvey/api/domain/QuestionId.java
@@ -0,0 +1,18 @@
+package com.thesurvey.api.domain;
+
+import java.io.Serializable;
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import lombok.Getter;
+
+@Embeddable
+@Getter
+public class QuestionId implements Serializable {
+
+    @Column(name = "survey_id")
+    private UUID surveyId;
+
+    @Column(name = "question_bank_id")
+    private Long questionBankId;
+}
