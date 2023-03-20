8cbba8527ae91cff833963cf5add7060191fbe6e
sanghyun1128 <sanghyun1128@gmail.com>
2023-03-20 17:00:38 +0900

    feat: get survey data from api

--- web/src/routes/SurveyPages/SurveyPage.tsx
+++ web/src/routes/SurveyPages/SurveyPage.tsx
@@ -1,12 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
+import { AxiosResponse, AxiosError } from 'axios';
 import { useParams } from 'react-router-dom';
 
+import axios from '../../api/axios';
+import requests from '../../api/request';
+
+interface QuestionOption {
+  option_number: number;
+  text: string;
+}
+
+interface SurveyQuestion {
+  question_id: number;
+  type: string;
+  title: string;
+  description: string;
+  options: Array<QuestionOption>;
+}
+
+interface SurveyData {
+  survey_id: string;
+  author: number;
+  title: string;
+  description: string;
+  created_date: string;
+  ended_date: string;
+  required_authentications: Array<string>;
+  questions: Array<SurveyQuestion>;
+}
+// TODO: over ended date while participant survey
 export default function SurveyPage() {
   const { id } = useParams();
+  const [surveyData, setSurveyData] = useState<SurveyData>();
+
+  const fetchSurveyData = async () => {
+    try {
+      const request: AxiosResponse<SurveyData> = await axios.get<SurveyData>(requests.fetchSurvey + id);
+      setSurveyData(request.data);
+    } catch (error) {
+      const { name } = error as unknown as AxiosError;
+    }
+  };
+
+  useEffect(() => {
+    fetchSurveyData();
+  }, []);
   return (
     <div>
-      <h1>{`SurveyPage : ${id}`}</h1>
+      <h1>{`${surveyData}`}</h1>
     </div>
   );
 }
