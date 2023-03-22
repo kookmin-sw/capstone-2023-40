f56b4486882db4bbed821d6f46741cce2940346e
JinMyeong-Kim-Kr <kjmdkdlel@gmail.com>
2023-03-23 01:33:52 +0900

    Refactor: Delete unnecessary constructor

--- api/src/main/java/com/thesurvey/api/service/SurveyService.java
+++ api/src/main/java/com/thesurvey/api/service/SurveyService.java
@@ -1,40 +1,21 @@
 package com.thesurvey.api.service;
 
-import com.thesurvey.api.domain.QuestionBank;
 import com.thesurvey.api.domain.Survey;
-import com.thesurvey.api.dto.QuestionBankDto;
-import com.thesurvey.api.dto.SurveyDto;
-import com.thesurvey.api.repository.QuestionBankRepository;
-import com.thesurvey.api.repository.QuestionOptionRepository;
 import com.thesurvey.api.repository.SurveyRepository;
-import com.thesurvey.api.service.mapper.QuestionBankMapper;
-import com.thesurvey.api.service.mapper.SurveyMapper;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import javax.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class SurveyService {
 
     private final SurveyRepository surveyRepository;
-    private final QuestionBankRepository questionBankRepository;
-    private final QuestionOptionRepository questionOptionRepository;
-    private final SurveyMapper surveyMapper;
-    private final QuestionBankMapper questionBankMapper;
 
 
-    public SurveyService(SurveyRepository surveyRepository,
-        QuestionBankRepository questionBankRepository,
-        QuestionOptionRepository questionOptionRepository, SurveyMapper surveyMapper,
-        QuestionBankMapper questionBankMapper) {
+    public SurveyService(SurveyRepository surveyRepository) {
         this.surveyRepository = surveyRepository;
-        this.questionBankRepository = questionBankRepository;
-        this.questionOptionRepository = questionOptionRepository;
-        this.surveyMapper = surveyMapper;
-        this.questionBankMapper = questionBankMapper;
     }
 
     public List<Survey> getAllSurvey() {
