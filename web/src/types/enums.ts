ec36a30a04980c33dad82a7b37ddb41a6966b2de
SeiwonPark <psw7347@gmail.com>
2023-03-24 03:04:05 +0900

    feat: add auth label key and log level types

--- web/src/types/enums.ts
+++ web/src/types/enums.ts
@@ -1,5 +1,6 @@
 export enum AuthLabel {
   KAKAO = '카카오계정',
+  NAVER = '네이버계정',
   GOOGLE = '구글계정',
   ID = '신분증',
   MOBILE_PHONE = '휴대폰',
@@ -7,3 +8,7 @@ export enum AuthLabel {
   WEBMAIL = '학교웹메일',
   NULL = '제한없음',
 }
+
+export type AuthLabelKey = keyof typeof AuthLabel;
+
+export type LogLevel = 'ERROR' | 'WARN' | 'INFO';
