7d3df87b49a9c1926b05acea8bbed05a615a3d06
SeiwonPark <psw7347@gmail.com>
2023-03-25 01:01:02 +0900

    chore: add assets

--- web/src/assets/svg/index.ts
+++ web/src/assets/svg/index.ts
@@ -1,5 +1,6 @@
 import { ReactComponent as DriverLicense } from './driver-license-icon.svg';
 import { ReactComponent as Error } from './error-icon.svg';
+import { ReactComponent as Favicon } from './favicon.svg';
 import { ReactComponent as Google } from './google-icon.svg';
 import { ReactComponent as Id } from './id-icon.svg';
 import { ReactComponent as Info } from './info-icon.svg';
@@ -8,26 +9,25 @@ import { ReactComponent as DarkLogo } from './logo-dark.svg';
 import { ReactComponent as LightLogo } from './logo-light.svg';
 import { ReactComponent as MobilePhone } from './mobile-phone-icon.svg';
 import { ReactComponent as Naver } from './naver-icon.svg';
+import { ReactComponent as Shortcut } from './shortcut-icon.svg';
 import { ReactComponent as Warn } from './warn-icon.svg';
 import { ReactComponent as Webmail } from './webmail-icon.svg';
 
-export const Icons: {
-  [src: string]: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement> & {
-      title?: string | undefined;
-    }
-  >;
-} = {
-  Kakao: Kakao,
-  Naver: Naver,
-  Google: Google,
-  Id: Id,
-  Webmail: Webmail,
-  DriverLicense: DriverLicense,
-  MobilePhone: MobilePhone,
-  Error: Error,
-  Info: Info,
-  Warn: Warn,
-  DarkLogo: DarkLogo,
-  LightLogo: LightLogo,
+export type IconType = React.FunctionComponent<React.SVGProps<SVGSVGElement> & { title?: string | undefined }>;
+
+export const Icons: { [src: string]: IconType } = {
+  DRIVER_LICENSE: DriverLicense,
+  ERROR: Error,
+  FAVICON: Favicon,
+  GOOGLE: Google,
+  ID: Id,
+  INFO: Info,
+  KAKAO: Kakao,
+  DARK_LOGO: DarkLogo,
+  LIGHT_LOGO: LightLogo,
+  MOBILE_PHONE: MobilePhone,
+  NAVER: Naver,
+  SHORTCUT: Shortcut,
+  WARN: Warn,
+  WEBMAIL: Webmail,
 };
