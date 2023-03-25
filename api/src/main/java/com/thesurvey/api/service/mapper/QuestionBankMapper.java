c6510b8993630aec8a021125bcf6211b4a61eec7
JinMyeong-Kim-Kr <kjmdkdlel@gmail.com>
2023-03-25 15:09:57 +0900

    Chore: make temporarily disable

--- api/src/main/java/com/thesurvey/api/service/mapper/QuestionBankMapper.java
+++ api/src/main/java/com/thesurvey/api/service/mapper/QuestionBankMapper.java
@@ -1,30 +1,30 @@
-package com.thesurvey.api.service.mapper;
-
-import com.thesurvey.api.domain.QuestionBank;
-import com.thesurvey.api.dto.QuestionBankDto;
-import java.util.Optional;
-import org.springframework.stereotype.Component;
-
-@Component
-public class QuestionBankMapper {
-
-    public QuestionBank toQuestionBank(QuestionBankDto questionBankDto) {
-        return QuestionBank.builder()
-            .title(questionBankDto.getTitle())
-            .question(questionBankDto.getQuestion())
-            .questionType(questionBankDto.getQuestionType())
-            .description(questionBankDto.getDescription())
-            .questionOptions(questionBankDto.getQuestionOptions())
-            .build();
-    }
-
-    public Optional<QuestionBankDto> toQuestionBankDto(Optional<QuestionBank> questionBank) {
-        return questionBank.map(value -> QuestionBankDto.builder()
-            .title(value.getTitle())
-            .question(value.getQuestion())
-            .questionType(value.getQuestionType())
-            .description(value.getDescription())
-            .questionOptions(value.getQuestionOptions())
-            .build());
-    }
-}
+//package com.thesurvey.api.service.mapper;
+//
+//import com.thesurvey.api.domain.QuestionBank;
+//import com.thesurvey.api.dto.QuestionBankDto;
+//import java.util.Optional;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class QuestionBankMapper {
+//
+//    public QuestionBank toQuestionBank(QuestionBankDto questionBankDto) {
+//        return QuestionBank.builder()
+//            .title(questionBankDto.getTitle())
+//            .question(questionBankDto.getQuestion())
+//            .type(questionBankDto.getQuestionType())
+//            .description(questionBankDto.getDescription())
+//            .questionOptions(questionBankDto.getQuestionOptionDtos())
+//            .build();
+//    }
+//
+//    public Optional<QuestionBankDto> toQuestionBankDto(Optional<QuestionBank> questionBank) {
+//        return questionBank.map(value -> QuestionBankDto.builder()
+//            .title(value.getTitle())
+//            .question(value.getQuestion())
+//            .questionType(value.getType())
+//            .description(value.getDescription())
+//            .questionOptionDtos(value.getQuestionOptions())
+//            .build());
+//    }
+//}
