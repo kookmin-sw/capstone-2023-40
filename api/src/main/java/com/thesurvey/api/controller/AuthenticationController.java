6a604dc311431b6a9eaad3554a20fe65c8185a1c
SeiwonPark <psw7347@gmail.com>
2023-03-29 18:09:29 +0900

    docs: add swagger docs for authentication controller

--- api/src/main/java/com/thesurvey/api/controller/AuthenticationController.java
+++ api/src/main/java/com/thesurvey/api/controller/AuthenticationController.java
@@ -1,6 +1,7 @@
 package com.thesurvey.api.controller;
 
 import com.thesurvey.api.dto.UserInfoDto;
+import com.thesurvey.api.dto.request.UserLoginRequestDto;
 import com.thesurvey.api.dto.request.UserRegisterRequestDto;
 import com.thesurvey.api.exception.ErrorMessage;
 import com.thesurvey.api.exception.ExceptionMapper;
@@ -12,6 +13,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@Api(tags = "인증")
 @RestController
 @RequestMapping("/auth")
 public class AuthenticationController {
@@ -22,8 +30,16 @@ public class AuthenticationController {
         this.authenticationService = authenticationService;
     }
 
+    @ApiOperation(value = "회원가입")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "요청 성공"),
+        @ApiResponse(code = 400, message = "잘못된 요청"),
+        @ApiResponse(code = 404, message = "요청한 리소스 찾을 수 없음"),
+        @ApiResponse(code = 500, message = "서버 내부 오류")
+    })
     @PostMapping("/register")
     public ResponseEntity<UserInfoDto> register(
+        @ApiParam(value = "UserRegisterRequestDto 객체", required = true)
         @RequestBody UserRegisterRequestDto userRegisterRequestDto) {
         try {
             return ResponseEntity.ok(authenticationService.register(userRegisterRequestDto));
@@ -33,11 +49,19 @@ public class AuthenticationController {
         }
     }
 
+    @ApiOperation(value = "로그인")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "요청 성공"),
+        @ApiResponse(code = 401, message = "유효하지 않은 인증정보"),
+        @ApiResponse(code = 403, message = "접근 권한 없음"),
+        @ApiResponse(code = 404, message = "요청한 리소스 찾을 수 없음")
+    })
     @PostMapping("/login")
-    public ResponseEntity<UserInfoDto> login(@RequestBody UserRegisterRequestDto userRequestDto) {
+    public ResponseEntity<UserInfoDto> login(
+        @ApiParam(value = "UserLoginRequestDto 객체", required = true)
+        @RequestBody UserLoginRequestDto userLoginRequestDto) {
         try {
-            UserInfoDto userInfoDto = authenticationService.login(userRequestDto.getEmail(),
-                userRequestDto.getPassword());
+            UserInfoDto userInfoDto = authenticationService.login(userLoginRequestDto);
             return ResponseEntity.ok(userInfoDto);
         } catch (AuthenticationException e) {
             throw new ExceptionMapper(ErrorMessage.UNAUTHORIZED_REQUEST);
