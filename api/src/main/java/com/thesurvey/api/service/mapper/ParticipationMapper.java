6f8672fa9db5c6c3222b829194720887d8f29875
SeiwonPark <psw7347@gmail.com>
2023-03-30 11:19:47 +0900

    feat: add mapper for participation

---
+++ api/src/main/java/com/thesurvey/api/service/mapper/ParticipationMapper.java
@@ -0,0 +1,24 @@
+package com.thesurvey.api.service.mapper;
+
+import com.thesurvey.api.domain.EnumTypeEntity.CertificationType;
+import com.thesurvey.api.domain.Participation;
+import com.thesurvey.api.domain.Survey;
+import com.thesurvey.api.domain.User;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ParticipationMapper {
+
+    public Participation toParticipation(User user, Survey survey,
+        CertificationType certificationType) {
+        return Participation.builder()
+            .user(user)
+            .survey(survey)
+            .certificationType(certificationType)
+            .participateDate(LocalDateTime.now(ZoneId.of("Asia/Seoul")))
+            .submittedDate(LocalDateTime.now(ZoneId.of("Asia/Seoul")))
+            .build();
+    }
+}
