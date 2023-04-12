f9982960e13025b826d847888ac12ce66a9782de
JinMyeong-Kim-Kr <kjmdkdlel@gmail.com>
2023-04-13 02:48:46 +0900

    feat: add validation logic and test code

--- api/src/main/java/com/thesurvey/api/service/SurveyService.java
+++ api/src/main/java/com/thesurvey/api/service/SurveyService.java
@@ -1,12 +1,17 @@
 package com.thesurvey.api.service;
 
 import com.thesurvey.api.domain.Survey;
+import com.thesurvey.api.domain.User;
 import com.thesurvey.api.dto.response.SurveyResponseDto;
 import com.thesurvey.api.dto.request.SurveyRequestDto;
 import com.thesurvey.api.dto.request.SurveyUpdateRequestDto;
 import com.thesurvey.api.exception.ErrorMessage;
-import com.thesurvey.api.exception.ExceptionMapper;
+import com.thesurvey.api.exception.BadRequestExceptionMapper;
+import com.thesurvey.api.exception.ForbiddenRequestExceptionMapper;
+import com.thesurvey.api.exception.NotFoundExceptionMapper;
+import com.thesurvey.api.repository.ParticipationRepository;
 import com.thesurvey.api.repository.SurveyRepository;
+import com.thesurvey.api.repository.UserRepository;
 import com.thesurvey.api.service.mapper.SurveyMapper;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -26,23 +31,28 @@ public class SurveyService {
     private final QuestionService questionService;
     private final ParticipationService participationService;
     private final AnsweredQuestionService answeredQuestionService;
+    private final UserRepository userRepository;
+    private final ParticipationRepository participationRepository;
 
     public SurveyService(SurveyRepository surveyRepository, SurveyMapper surveyMapper,
         QuestionService questionService,
         ParticipationService participationService,
-        AnsweredQuestionService answeredQuestionService) {
+        AnsweredQuestionService answeredQuestionService, UserRepository userRepository,
+        ParticipationRepository participationRepository) {
         this.surveyRepository = surveyRepository;
         this.surveyMapper = surveyMapper;
         this.questionService = questionService;
         this.participationService = participationService;
         this.answeredQuestionService = answeredQuestionService;
+        this.userRepository = userRepository;
+        this.participationRepository = participationRepository;
     }
 
     @Transactional(readOnly = true)
     public List<SurveyResponseDto> getAllSurvey() {
         return Optional.ofNullable(
                 surveyRepository.findAllInDescendingOrder())
-            .orElseThrow(() -> new ExceptionMapper(ErrorMessage.SURVEY_NOT_FOUND))
+            .orElseThrow(() -> new NotFoundExceptionMapper(ErrorMessage.SURVEY_NOT_FOUND))
             .stream()
             .map(surveyMapper::toSurveyResponseDto)
             .collect(Collectors.toList());
@@ -51,26 +61,56 @@ public class SurveyService {
     @Transactional(readOnly = true)
     public SurveyResponseDto getSurveyBySurveyIdWithRelatedQuestion(UUID surveyId) {
         Survey survey = surveyRepository.findBySurveyId(surveyId)
-            .orElseThrow(() -> new ExceptionMapper(ErrorMessage.SURVEY_NOT_FOUND));
+            .orElseThrow(() -> new NotFoundExceptionMapper(ErrorMessage.SURVEY_NOT_FOUND));
         return surveyMapper.toSurveyResponseDto(survey);
     }
 
     @Transactional
     public SurveyResponseDto createSurvey(Authentication authentication,
         SurveyRequestDto surveyRequestDto) {
-        validateRequestedSurveyDate(surveyRequestDto.getStartedDate(),
-            surveyRequestDto.getEndedDate());
+        /*
+         * A 5-second delay is allowed
+         * if a user sets the start time for a survey to the current time.
+         */
+        if (surveyRequestDto.getStartedDate().isBefore(
+            LocalDateTime.now(ZoneId.of("Asia/Seoul")).minusSeconds(5))) {
+            throw new BadRequestExceptionMapper(ErrorMessage.STARTEDDATE_ISBEFORE_CURRENTDATE);
+        }
+
+        // validate for when the start time of the survey is set after the end time.
+        if (surveyRequestDto.getStartedDate().isAfter(
+            surveyRequestDto.getEndedDate())) {
+            throw new BadRequestExceptionMapper(ErrorMessage.STARTEDDATE_ISAFTER_ENDEDDATE);
+        }
 
         Survey survey = surveyRepository.save(surveyMapper.toSurvey(surveyRequestDto));
         questionService.createQuestion(surveyRequestDto, survey);
-        participationService.createParticipation(authentication, surveyRequestDto, survey);
+        participationService.createParticipation(authentication,
+            surveyRequestDto.getCertificationType(), survey);
         return surveyMapper.toSurveyResponseDto(survey);
     }
 
     @Transactional
-    public void deleteSurvey(UUID surveyId) {
+    public void deleteSurvey(Authentication authentication, UUID surveyId) {
         Survey survey = surveyRepository.findById(surveyId)
-            .orElseThrow(() -> new ExceptionMapper(ErrorMessage.SURVEY_NOT_FOUND, surveyId));
+            .orElseThrow(
+                () -> new NotFoundExceptionMapper(ErrorMessage.SURVEY_NOT_FOUND, surveyId));
+
+        User user = userRepository.findByName(authentication.getName())
+            .orElseThrow(() -> new NotFoundExceptionMapper(ErrorMessage.USER_NAME_NOT_FOUND,
+                authentication.getName()));
+
+        // validate for when a non-survey author attempts to delete.
+        if (!participationRepository.existsByUserIdAndSurveyId(user.getUserId(),
+            survey.getSurveyId())) {
+            throw new ForbiddenRequestExceptionMapper(ErrorMessage.NO_HAVE_AUTHORITY);
+        }
+
+        // validate for attempts to delete a started survey.
+        if (survey.getStartedDate().isBefore(LocalDateTime.now(ZoneId.of("Asia/Seoul")))
+            && LocalDateTime.now(ZoneId.of("Asia/Seoul")).isBefore(survey.getEndedDate())) {
+            throw new BadRequestExceptionMapper(ErrorMessage.SURVEY_ALREADY_STARTED);
+        }
 
         answeredQuestionService.deleteAnswer(surveyId);
         surveyRepository.delete(survey);
@@ -79,21 +119,29 @@ public class SurveyService {
     }
 
     @Transactional
-    public SurveyResponseDto updateSurvey(SurveyUpdateRequestDto surveyUpdateRequestDto) {
-        validateRequestedSurveyDate(surveyUpdateRequestDto.getStartedDate(),
-            surveyUpdateRequestDto.getEndedDate());
+    public SurveyResponseDto updateSurvey(Authentication authentication,
+        SurveyUpdateRequestDto surveyUpdateRequestDto) {
+        Survey survey = surveyRepository.findById(surveyUpdateRequestDto.getSurveyId())
+            .orElseThrow(() -> new NotFoundExceptionMapper(ErrorMessage.SURVEY_NOT_FOUND,
+                surveyUpdateRequestDto.getSurveyId()));
 
-        Survey survey = surveyRepository.findBySurveyId(surveyUpdateRequestDto.getSurveyId())
-            .orElseThrow(
-                () -> new ExceptionMapper(ErrorMessage.SURVEY_NOT_FOUND,
-                    surveyUpdateRequestDto.getSurveyId()));
+        User user = userRepository.findByName(authentication.getName())
+            .orElseThrow(() -> new NotFoundExceptionMapper(ErrorMessage.USER_NAME_NOT_FOUND,
+                authentication.getName()));
+
+        if (!participationRepository.existsByUserIdAndSurveyId(user.getUserId(),
+            survey.getSurveyId())) {
+            throw new ForbiddenRequestExceptionMapper(ErrorMessage.NO_HAVE_AUTHORITY);
+        }
+
+        validateUpdateSurvey(survey, surveyUpdateRequestDto);
 
         if (surveyUpdateRequestDto.getTitle() != null) {
-            survey.changeTitle(surveyUpdateRequestDto.getTitle());
+            survey.changeTitle(surveyUpdateRequestDto.getTitle().trim());
         }
 
         if (surveyUpdateRequestDto.getDescription() != null) {
-            survey.changeDescription(surveyUpdateRequestDto.getDescription());
+            survey.changeDescription(surveyUpdateRequestDto.getDescription().trim());
         }
 
         if (surveyUpdateRequestDto.getStartedDate() != null) {
@@ -105,16 +153,28 @@ public class SurveyService {
         }
 
         questionService.updateQuestion(surveyUpdateRequestDto.getQuestions());
-        surveyRepository.save(survey);
         return surveyMapper.toSurveyResponseDto(survey);
     }
 
-    public void validateRequestedSurveyDate(LocalDateTime startedDate, LocalDateTime endedDate) {
-        if (startedDate.isBefore(LocalDateTime.now(ZoneId.of("Asia/Seoul")).minusSeconds(5))) {
-            throw new ExceptionMapper(ErrorMessage.REQUEST_TIMEOUT);
+    public void validateUpdateSurvey(Survey survey, SurveyUpdateRequestDto surveyUpdateRequestDto) {
+        // validate for attempts to modify ended survey.
+        if (survey.getEndedDate().isBefore(LocalDateTime.now(ZoneId.of("Asia/Seoul")))) {
+            throw new BadRequestExceptionMapper(ErrorMessage.SURVEY_ALREADY_ENDED);
         }
-        if (startedDate.isAfter(endedDate)) {
-            throw new ExceptionMapper(ErrorMessage.STARTEDDATE_ISAFTER_ENDEDDATE);
+
+        // validate for when to modify a survey that has already been started.
+        if (survey.getStartedDate()
+            .isBefore(LocalDateTime.now(ZoneId.of("Asia/Seoul")).minusSeconds(5))
+            && LocalDateTime.now(ZoneId.of("Asia/Seoul")).isBefore(survey.getEndedDate())) {
+            throw new BadRequestExceptionMapper(ErrorMessage.SURVEY_ALREADY_STARTED);
         }
+
+        // validate for when the start time of the survey is set after the end time.
+        if (surveyUpdateRequestDto.getEndedDate()
+            .isBefore(surveyUpdateRequestDto.getStartedDate())) {
+            throw new BadRequestExceptionMapper(ErrorMessage.STARTEDDATE_ISAFTER_ENDEDDATE);
+        }
+
     }
+
 }
--- api/src/test/java/com/thesurvey/api/service/SurveyServiceTest.java
+++ api/src/test/java/com/thesurvey/api/service/SurveyServiceTest.java
@@ -1,22 +1,35 @@
 package com.thesurvey.api.service;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import com.thesurvey.api.domain.Survey;
+import com.thesurvey.api.domain.User;
+import com.thesurvey.api.dto.request.SurveyRequestDto;
+import com.thesurvey.api.dto.request.SurveyUpdateRequestDto;
 import com.thesurvey.api.dto.response.SurveyResponseDto;
-import com.thesurvey.api.exception.ExceptionMapper;
+import com.thesurvey.api.exception.BadRequestExceptionMapper;
+import com.thesurvey.api.exception.ErrorMessage;
+import com.thesurvey.api.repository.ParticipationRepository;
 import com.thesurvey.api.repository.SurveyRepository;
+import com.thesurvey.api.repository.UserRepository;
 import com.thesurvey.api.service.mapper.SurveyMapper;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 
 @ExtendWith(MockitoExtension.class)
 public class SurveyServiceTest {
@@ -26,6 +39,18 @@ public class SurveyServiceTest {
 
     @Mock
     SurveyMapper surveyMapper;
+
+    @Mock
+    UserRepository userRepository;
+
+    @Mock
+    QuestionService questionService;
+
+    @Mock
+    ParticipationService participationService;
+
+    @Mock
+    ParticipationRepository participationRepository;
 
     @InjectMocks
     SurveyService surveyService;
@@ -66,23 +91,258 @@ public class SurveyServiceTest {
     }
 
     @Test
-    void testInvalidTimeRequest() {
-        LocalDateTime currentTime = LocalDateTime.now();
-        Survey startedDateIsBeforeCurrentSurvey = Survey.builder()
+    void testCorrectCreateSurveyRequest() {
+        // given
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+            "test", "test");
+
+        SurveyRequestDto testSurveyRequestDto = SurveyRequestDto.builder()
+            .title("test survey title")
+            .startedDate(LocalDateTime.now().plusDays(1))
+            .endedDate(LocalDateTime.now().plusDays(2))
+            .build();
+
+        SurveyResponseDto expectSurveyResponseDto = SurveyResponseDto.builder()
+            .title("test survey title")
+            .build();
+
+        doNothing().when(questionService).createQuestion(any(), any());
+        doNothing().when(participationService).createParticipation(any(), any(), any());
+        when(surveyMapper.toSurveyResponseDto(any())).thenReturn(expectSurveyResponseDto);
+
+        // when
+        SurveyResponseDto result = surveyService.createSurvey(authentication, testSurveyRequestDto);
+
+        // then
+        assertEquals(expectSurveyResponseDto.getTitle(), result.getTitle());
+
+    }
+
+    @Test
+    void testCorrectUpdateSurveyRequest() {
+        // given
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+            "test", "test");
+
+        Survey testSurvey = Survey.builder()
+            .title("test survey")
+            .description("test survey description")
+            .startedDate(LocalDateTime.now().plusDays(1))
+            .endedDate(LocalDateTime.now().plusDays(2))
+            .build();
+
+        User testUser = User.builder()
+            .name("test user")
+            .build();
+
+        SurveyUpdateRequestDto surveyUpdateRequestDto = SurveyUpdateRequestDto.builder()
+            .title("test update survey")
+            .description("test update survey description")
+            .startedDate(LocalDateTime.now().plusDays(3))
+            .endedDate(LocalDateTime.now().plusDays(4))
+            .build();
+
+        SurveyResponseDto updatedSurveyDto = SurveyResponseDto.builder()
+            .title("test update survey")
+            .description("test update survey description")
+            .startedDate(LocalDateTime.now().plusDays(3))
+            .endedDate(LocalDateTime.now().plusDays(4))
+            .build();
+
+        when(surveyRepository.findById(surveyUpdateRequestDto.getSurveyId())).thenReturn(
+            Optional.ofNullable(testSurvey));
+        when(userRepository.findByName(authentication.getName())).thenReturn(
+            Optional.ofNullable(testUser));
+        when(participationRepository.existsByUserIdAndSurveyId(any(), any())).thenReturn(true);
+        when(surveyMapper.toSurveyResponseDto(any())).thenReturn(updatedSurveyDto);
+        doNothing().when(questionService).updateQuestion(any());
+
+        // when
+        SurveyResponseDto resultSurveyDto = surveyService.updateSurvey(authentication,
+            surveyUpdateRequestDto);
+
+        // then
+        assertEquals(updatedSurveyDto.getTitle(), resultSurveyDto.getTitle());
+
+    }
+
+    @Test
+    void testValidateCreateSurveyWithInvalidTimeRequest() {
+        // given
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+            "test", "test");
+
+        SurveyRequestDto testSurveyRequestDto = SurveyRequestDto.builder()
+            .title("Survey that startedDate is After endedDate.")
+            .startedDate(LocalDateTime.now().plusDays(1)) // set after endedDate
+            .endedDate(LocalDateTime.now().minusDays(1))
+            .build();
+
+        // when
+        BadRequestExceptionMapper result = assertThrows(BadRequestExceptionMapper.class,
+            () -> surveyService.createSurvey(authentication,
+                testSurveyRequestDto));
+
+        // then
+        assertEquals(ErrorMessage.STARTEDDATE_ISAFTER_ENDEDDATE.getMessage(), result.getMessage());
+    }
+
+    @Test
+    void testValidateCreateSurveyWithOverFiveSeconds() {
+        // given
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+            "test", "test");
+
+        SurveyRequestDto testSurveyRequestDto = SurveyRequestDto.builder()
             .title("Survey that startedDate is before current time over 5 seconds.")
             .startedDate(LocalDateTime.now().minusSeconds(6))
             .build();
 
-        Survey startDateIsAfterEndedDateSurvey = Survey.builder()
-            .title("Survey that startedDate is after endedDate")
-            .startedDate(LocalDateTime.now().plusHours(2))
-            .endedDate(LocalDateTime.now().plusHours(1))
+        // when
+        BadRequestExceptionMapper result = assertThrows(BadRequestExceptionMapper.class,
+            () -> surveyService.createSurvey(authentication,
+                testSurveyRequestDto));
+
+        // then
+        assertEquals(ErrorMessage.STARTEDDATE_ISBEFORE_CURRENTDATE.getMessage(), result.getMessage());
+
+    }
+
+    @Test
+    void testValidateUpdateSurveyAlreadyStarted() {
+        // given
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+            "test", "test");
+
+        SurveyUpdateRequestDto testSurveyUpdateRequestDto = SurveyUpdateRequestDto.builder()
+            .title("test survey update request dto")
             .build();
 
-        assertThrows(ExceptionMapper.class, () -> surveyService.validateRequestedSurveyDate(
-            startedDateIsBeforeCurrentSurvey.getStartedDate(), currentTime));
-        assertThrows(ExceptionMapper.class, () -> surveyService.validateRequestedSurveyDate(
-            startDateIsAfterEndedDateSurvey.getStartedDate(),
-            startDateIsAfterEndedDateSurvey.getEndedDate()));
+        Survey testSurvey = Survey.builder()
+            .title("test survey")
+            .startedDate(LocalDateTime.now()
+                .minusDays(1))  // set for survey that have already been started
+            .endedDate(LocalDateTime.now().plusDays(1))
+            .build();
+
+        User testUser = User.builder()
+            .name("test user")
+            .build();
+
+        when(surveyRepository.findById(any())).thenReturn(Optional.ofNullable(testSurvey));
+        when(userRepository.findByName(any())).thenReturn(Optional.ofNullable(testUser));
+        when(participationRepository.existsByUserIdAndSurveyId(any(), any())).thenReturn(true);
+
+        // when
+        BadRequestExceptionMapper result = assertThrows(BadRequestExceptionMapper.class,
+            () -> surveyService.updateSurvey(authentication,
+                testSurveyUpdateRequestDto));
+
+        // then
+        assertEquals(ErrorMessage.SURVEY_ALREADY_STARTED.getMessage(), result.getMessage());
     }
+
+    @Test
+    void testValidateUpdateSurveyWithInvalidTimeRequest() {
+        // given
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+            "test", "test");
+
+        SurveyUpdateRequestDto testSurveyUpdateRequestDto = SurveyUpdateRequestDto.builder()
+            .title("test survey update request dto")
+            .startedDate(LocalDateTime.now().plusDays(5)) // set after endedDate
+            .endedDate(LocalDateTime.now().plusDays(4))
+            .build();
+
+        Survey testSurvey = Survey.builder()
+            .title("test survey")
+            .startedDate(LocalDateTime.now().plusDays(1))
+            .endedDate(LocalDateTime.now().plusDays(2))
+            .build();
+
+        User testUser = User.builder()
+            .name("test user")
+            .build();
+
+        when(surveyRepository.findById(any())).thenReturn(Optional.ofNullable(testSurvey));
+        when(userRepository.findByName(any())).thenReturn(Optional.ofNullable(testUser));
+        when(participationRepository.existsByUserIdAndSurveyId(any(), any())).thenReturn(true);
+
+        // when
+        BadRequestExceptionMapper result = assertThrows(BadRequestExceptionMapper.class,
+            () -> surveyService.updateSurvey(authentication,
+                testSurveyUpdateRequestDto));
+
+        // then
+        assertEquals(ErrorMessage.STARTEDDATE_ISAFTER_ENDEDDATE.getMessage(), result.getMessage());
+    }
+
+    @Test
+    void testValidateUpdateSurveyAlreadyEnded() {
+        // given
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+            "test", "test");
+
+        SurveyUpdateRequestDto testSurveyUpdateRequestDto = SurveyUpdateRequestDto.builder()
+            .title("test survey update request dto")
+            .startedDate(LocalDateTime.now().plusDays(4))
+            .endedDate(LocalDateTime.now().plusDays(5))
+            .build();
+
+        Survey testSurvey = Survey.builder()
+            .title("test survey")
+            .startedDate(LocalDateTime.now().minusDays(1))
+            .endedDate(
+                LocalDateTime.now().minusSeconds(10)) // set for survey that have already been ended
+            .build();
+
+        User testUser = User.builder()
+            .name("test user")
+            .build();
+
+        when(surveyRepository.findById(any())).thenReturn(Optional.ofNullable(testSurvey));
+        when(userRepository.findByName(any())).thenReturn(Optional.ofNullable(testUser));
+        when(participationRepository.existsByUserIdAndSurveyId(any(), any())).thenReturn(true);
+
+        // when
+        BadRequestExceptionMapper result = assertThrows(BadRequestExceptionMapper.class,
+            () -> surveyService.updateSurvey(authentication,
+                testSurveyUpdateRequestDto));
+
+        // then
+        assertEquals(ErrorMessage.SURVEY_ALREADY_ENDED.getMessage(), result.getMessage());
+    }
+
+    @Test
+    void testValidateDeleteSurveyAlreadyStarted() {
+        // given
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+            "test", "test");
+
+        Survey testSurvey = Survey.builder()
+            .title("test survey")
+            .startedDate(LocalDateTime.now().minusDays(1))
+            .endedDate(LocalDateTime.now().plusDays(10))
+            .build();
+
+        User testUser = User.builder()
+            .name("test user")
+            .build();
+
+        when(surveyRepository.findById(any())).thenReturn(
+            Optional.ofNullable(testSurvey));
+        when(userRepository.findByName(any())).thenReturn(
+            Optional.ofNullable(testUser));
+        when(participationRepository.existsByUserIdAndSurveyId(any(), any())).thenReturn(true);
+
+        // when
+        BadRequestExceptionMapper result = assertThrows(BadRequestExceptionMapper.class,
+            () -> surveyService.deleteSurvey(authentication,
+                UUID.randomUUID()));
+
+        // then
+        assertEquals(ErrorMessage.SURVEY_ALREADY_STARTED.getMessage(), result.getMessage());
+
+    }
+
 }
