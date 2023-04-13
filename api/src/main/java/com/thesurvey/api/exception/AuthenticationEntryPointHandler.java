ed873646785c33240da600fb90c26b047df1cf21
JinMyeong-Kim-Kr <kjmdkdlel@gmail.com>
2023-04-13 15:47:21 +0900

    comment: add comment on AuthenticationEntryPointHandler.java

--- api/src/main/java/com/thesurvey/api/exception/AuthenticationEntryPointHandler.java
+++ api/src/main/java/com/thesurvey/api/exception/AuthenticationEntryPointHandler.java
@@ -5,24 +5,22 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
+/**
+ * Handles authentication errors that occur during the Spring Security filter chain, such as when a
+ * user attempts to access a secured endpoint without authentication credentials.
+ */
 public class AuthenticationEntryPointHandler implements AuthenticationEntryPoint {
 
+    // method is called by the Spring Security filter chain when an authentication error occurs.
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
         AuthenticationException authException) throws IOException, ServletException {
-            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-            if (authentication == null) {
-                response.setStatus(HttpStatus.UNAUTHORIZED.value());
-            } else {
-                response.setStatus(HttpStatus.FORBIDDEN.value());
-            }
-            response.setContentType("text/plain;charset=UTF-8");
-            response.setCharacterEncoding("UTF-8");
-            response.getWriter().write("권한이 없습니다.");
-        }
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType("text/plain;charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write("권한이 없습니다.");
     }
+}
