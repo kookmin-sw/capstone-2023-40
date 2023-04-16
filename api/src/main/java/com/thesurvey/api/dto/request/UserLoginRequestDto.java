3fc8fd99cd23a3ffd037c412d306b2c6ef478369
SeiwonPark <psw7347@gmail.com>
2023-04-16 18:12:07 +0900

    docs: updated swagger docs

--- api/src/main/java/com/thesurvey/api/dto/request/UserLoginRequestDto.java
+++ api/src/main/java/com/thesurvey/api/dto/request/UserLoginRequestDto.java
@@ -19,12 +19,12 @@ public class UserLoginRequestDto {
     @Email
     @NotBlank
     @Size(max = 100)
-    @ApiModelProperty(name = "이메일", example = "test@gmail.com")
+    @ApiModelProperty(name = "이메일", example = "test@gmail.com", notes = "이메일은 100자 이내여야 합니다.")
     private String email;
 
     @NotBlank
     @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[~!@#$%^&*()_+=])\\S{8,25}$")
-    @ApiModelProperty(name = "비밀번호", example = "Mypassword123@")
+    @ApiModelProperty(name = "비밀번호", example = "Mypassword123@", notes = "비밀번호는 대소문자 및 숫자, 그리고 특수문자를 최소 하나 이상 포함해야 하며, 8자 이상 25자 이하여야 합니다.")
     private String password;
 
 }
