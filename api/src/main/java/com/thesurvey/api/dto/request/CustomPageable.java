00edca3f9e4f308607361e61f44fddfaf34a3d4e
JinMyeong-Kim-Kr <kjmdkdlel@gmail.com>
2023-04-21 03:54:31 +0900

    refactor: remove pageSize field

--- api/src/main/java/com/thesurvey/api/dto/request/CustomPageable.java
+++ api/src/main/java/com/thesurvey/api/dto/request/CustomPageable.java
@@ -8,9 +8,6 @@ import lombok.Getter;
 @ApiModel
 public class CustomPageable {
 
-    @ApiModelProperty(name = "페이지 크기", example = "한 페이지에 포함될 항목의 개수입니다.", notes = "1 부터 시작하며, 유효하지 않은 값은 1로 설정됩니다.")
-    private Integer pageSize;
-
     @ApiModelProperty(name = "페이지 수", example = "가져올 페이지의 번호를 나타냅니다.", notes = "0 부터 시작하며, 유효하지 않은 값은 0으로 설정됩니다.")
     private Integer pageNum;
 }
