ab0de5e3cca0a0e6ace00f202bc74aabb4c7b7d9
SeiwonPark <psw7347@gmail.com>
2023-04-22 16:12:44 +0900

    fix: response dto docs

--- api/src/main/java/com/thesurvey/api/dto/response/SurveyResponseDto.java
+++ api/src/main/java/com/thesurvey/api/dto/response/SurveyResponseDto.java
@@ -1,6 +1,7 @@
 package com.thesurvey.api.dto.response;
 
 import com.thesurvey.api.domain.EnumTypeEntity.CertificationType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
@@ -11,16 +12,22 @@ import lombok.Getter;
 @Builder
 public class SurveyResponseDto {
 
+    @Schema(example = "93c0a231-207e-4190-aee9-0a5f78cafc44", description = "생성된 설문조사의 아이디입니다.")
     private UUID surveyId;
 
+    @Schema(example = "1", description = "설문조사 생성자의 아이디입니다.")
     private Long authorId;
 
+    @Schema(example = "카카오 사용자분들께 설문 부탁드립니다!", description = "생성된 설문조사의 제목입니다.")
     private String title;
 
+    @Schema(example = "카카오 앱 서비스에 대한 전반적인 만족도 조사입니다.", description = "생성된 설문조사의 제목입니다.")
     private String description;
 
+    @Schema(example = "2030-12-01T00:00:00", description = "생성된 설문조사의 시작일입니다.")
     private LocalDateTime startedDate;
 
+    @Schema(example = "2030-12-12T00:00:00", description = "생성된 설문조사의 종료일입니다.")
     private LocalDateTime endedDate;
 
     private List<QuestionBankResponseDto> questions;
@@ -29,6 +36,7 @@ public class SurveyResponseDto {
 
     private LocalDateTime modifiedDate;
 
+    @Schema(example = "[\"NAVER\", \"KAKAO\"]", description = "생성된 설문조사의 필수인증 목록입니다.")
     private List<CertificationType> certificationTypes;
 
 }
--- api/src/main/java/com/thesurvey/api/dto/response/UserResponseDto.java
+++ api/src/main/java/com/thesurvey/api/dto/response/UserResponseDto.java
@@ -1,6 +1,7 @@
 package com.thesurvey.api.dto.response;
 
 import com.thesurvey.api.domain.EnumTypeEntity.Role;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,22 +10,31 @@ import lombok.Getter;
 @Builder
 public class UserResponseDto {
 
+    @Schema(example = "1", description = "사용자 아이디")
     private Long userId;
 
+    @Schema(example = "test@example.com", description = "이메일")
     private String email;
 
+    @Schema(example = "test", description = "이름")
     private String name;
 
+    @Schema(example = "01012341234", description = "휴대폰 번호")
     private String phoneNumber;
 
+    @Schema(example = "대한민국 서울", description = "주소")
     private String address;
 
+    @Schema(example = "USER", description = "사용자 역할")
     private Role role;
 
+    @Schema(example = "https://images2.alphacoders.com/130/1306410.png", description = "프로필 이미지 URL")
     private String profileImage;
 
+    @Schema(example = "2030-12-01T00:00:00", description = "계정 생성일")
     private LocalDateTime createdDate;
 
+    @Schema(example = "2030-12-12T00:00:00", description = "계정 수정일")
     private LocalDateTime modifiedDate;
 
 }
