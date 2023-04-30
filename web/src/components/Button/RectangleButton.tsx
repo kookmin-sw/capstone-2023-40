293fbd8d26577cd0ee562deea90bea8133d674dc
sanghyun1128 <sanghyun1128@gmail.com>
2023-04-30 20:53:25 +0900

    feat: setable font option

--- web/src/components/Button/RectangleButton.tsx
+++ web/src/components/Button/RectangleButton.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 
 import styled, { DefaultTheme } from 'styled-components';
 
-const Button = styled.button<{ width: string; backgroundColor: string; backgroundHover: string }>`
+const Button = styled.button<{
+  width: string;
+  backgroundColor: string;
+  backgroundHover: string;
+  fontSize: string;
+  fontWeight: string;
+}>`
   padding: 2vh 2vw 2vh 2vw;
   width: ${(props) => props.width};
-  font-size: 18px;
-  font-weight: 700;
+  font-size: ${(props) => props.fontSize};
+  font-weight: ${(props) => props.fontWeight};
   border: none;
   border-radius: ${(props) => props.theme.borderRadius};
   color: ${(props) => props.theme.colors.default};
@@ -24,6 +30,8 @@ interface RectangleButtonProps {
   width: string;
   backgroundColor: string;
   hoverColor: string;
+  fontSize?: string;
+  fontWeight?: string;
   handleClick: () => void;
   theme: DefaultTheme;
 }
@@ -33,6 +41,8 @@ export default function RectangleButton({
   width,
   backgroundColor,
   hoverColor,
+  fontSize,
+  fontWeight,
   handleClick,
   theme,
 }: RectangleButtonProps) {
@@ -42,6 +52,8 @@ export default function RectangleButton({
       width={width}
       backgroundColor={backgroundColor}
       backgroundHover={hoverColor}
+      fontSize={fontSize || '18px'}
+      fontWeight={fontWeight || '600'}
       onClick={handleClick}
     >
       {text}
