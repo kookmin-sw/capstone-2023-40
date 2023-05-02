9a8ce312c952c3cc0b2f0879df7f41530b543339
오성혁 <68771488+ohsung1125@users.noreply.github.com>
2023-05-02 20:45:33 +0900

    feat: modified surveyAuth reducer & add complete authPage

--- web/src/types/surveyAuth.ts
+++ web/src/types/surveyAuth.ts
@@ -7,10 +7,48 @@ export type SurveyAuthState = {
   webmail: boolean;
 };
 
+export const setAuthKakao = (payload: boolean) => ({
+  type: 'AUTH_KAKAO',
+  payload: !payload,
+});
+
+export const setAuthGoogle = (payload: boolean) => ({
+  type: 'AUTH_GOOGLE',
+  payload: !payload,
+});
+
+export const setAuthNaver = (payload: boolean) => ({
+  type: 'AUTH_NAVER',
+  payload: !payload,
+});
+
+export const setAuthIdentity = (payload: boolean) => ({
+  type: 'AUTH_IDENTITY',
+  payload: !payload,
+});
+
+export const setAuthDriver = (payload: boolean) => ({
+  type: 'AUTH_DRIVER',
+  payload: !payload,
+});
+
+export const setAuthWebMail = (payload: boolean) => ({
+  type: 'AUTH_WEBMAIL',
+  payload: !payload,
+});
+
+// export type SurveyAuthAction =
+//   | { type: 'AUTH_KAKAO'; payload: boolean }
+//   | { type: 'AUTH_NAVER'; payload: boolean }
+//   | { type: 'AUTH_GOOGLE'; payload: boolean }
+//   | { type: 'AUTH_IDENTITY'; payload: boolean }
+//   | { type: 'AUTH_DRIVER'; payload: boolean }
+//   | { type: 'AUTH_WEBMAIL'; payload: boolean };
+
 export type SurveyAuthAction =
-  | { type: 'AUTH_KAKAO'; payload: boolean }
-  | { type: 'AUTH_NAVER'; payload: boolean }
-  | { type: 'AUTH_GOOGLE'; payload: boolean }
-  | { type: 'AUTH_IDENTITY'; payload: boolean }
-  | { type: 'AUTH_DRIVER'; payload: boolean }
-  | { type: 'AUTH_WEBMAIL'; payload: boolean };
+  | ReturnType<typeof setAuthKakao>
+  | ReturnType<typeof setAuthGoogle>
+  | ReturnType<typeof setAuthNaver>
+  | ReturnType<typeof setAuthIdentity>
+  | ReturnType<typeof setAuthDriver>
+  | ReturnType<typeof setAuthWebMail>;
