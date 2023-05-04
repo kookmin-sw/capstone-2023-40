07ce39829c77a249396e812f7d6546925cbb3940
sanghyun1128 <sanghyun1128@gmail.com>
2023-05-05 06:30:02 +0900

    fix: remove mock data

--- web/src/routes/SurveyPages/SurveyListPage.tsx
+++ web/src/routes/SurveyPages/SurveyListPage.tsx
@@ -32,90 +32,13 @@ export default function SurveyListPage() {
   const fetchSurveyList = async (abortSignal: AbortSignal): Promise<void> => {
     setIsLoading(true);
     try {
-      // const request: AxiosResponse<SurveyPageResponse> = await axios.get<SurveyPageResponse>(
-      //   `${requests.getSurvey}${page}`,
-      //   { signal: abortSignal, }
-      // );
-      const request = {
-        surveys: [
-          {
-            surveyId: 'b4458690-3b83-4929-81b2-f6d27743dbb2',
-            authorId: 3,
-            title: '카카오 사용자분들 대상으로 한 설문조사입니다!',
-            description: '간단한 설문조사입니다.',
-            startedDate: '2023-04-30T15:54:00',
-            endedDate: '2023-05-20T23:59:59',
-            certificationTypes: [],
-            createdDate: '2023-04-28T23:29:31',
-            modifiedDate: '2023-04-30T00:02:39',
-          },
+      const request: AxiosResponse<SurveyPageResponse> = await axios.get<SurveyPageResponse>(
+        `${requests.getSurveyPage}${page}`,
+        { signal: abortSignal }
+      );
 
-          {
-            surveyId: '34b2a64a-39a5-4c3f-9e93-5d4201e7f748',
-            authorId: 3,
-            title: '카카오 사용자분들 대상으로 한 설문조사입니다!',
-            description: '간단한 설문조사입니다.',
-            startedDate: '2023-04-30T15:54:00',
-            endedDate: '2023-05-20T23:59:59',
-            certificationTypes: [1, 2, 3, 4],
-            createdDate: '2023-04-28T23:29:31',
-            modifiedDate: '2023-04-30T00:02:13',
-          },
-
-          {
-            surveyId: '1eb00f9e-438e-41ba-a647-6490f0c43b8b',
-            authorId: 3,
-            title: '카카오 사용자분들 대상으로 한 설문조사입니다!',
-            description: '간단한 설문조사입니다.',
-            startedDate: '2023-04-30T15:54:00',
-            endedDate: '2023-05-20T23:59:59',
-            certificationTypes: [0],
-            createdDate: '2023-04-28T23:29:31',
-            modifiedDate: '2023-04-30T00:01:04',
-          },
-
-          {
-            surveyId: '768ac9c6-de4a-4c40-b21b-b2ba86c0fab5',
-            authorId: 1,
-            title: '(2) 카카오 사용자분들 대상으로 한 설문조사입니다!',
-            description: '간단한 설문조사입니다.',
-            startedDate: '2023-04-30T15:54:00',
-            endedDate: '2023-05-20T23:59:59',
-            certificationTypes: [1],
-            createdDate: '2023-04-28T23:29:31',
-            modifiedDate: '2023-04-29T00:11:26',
-          },
-
-          {
-            surveyId: '6002842b-7fc5-415e-a280-b226f2b25baa',
-            authorId: 3,
-            title: '카카오 사용자분들 대상으로 한 설문조사입니다!',
-            description: '간단한 설문조사입니다.',
-            startedDate: '2023-04-30T15:54:00',
-            endedDate: '2023-05-20T23:59:59',
-            certificationTypes: [3],
-            createdDate: '2023-04-28T23:29:31',
-            modifiedDate: '2023-04-28T23:54:24',
-          },
-
-          {
-            surveyId: '5219dd76-c208-47a2-bf3c-ad3d7faded15',
-            authorId: 1,
-            title: '카카오 사용자분들 대상으로 한 설문조사입니다!',
-            description: '간단한 설문조사입니다.',
-            startedDate: '2023-04-30T15:54:00',
-            endedDate: '2023-05-20T23:59:59',
-            certificationTypes: [4],
-            createdDate: '2023-04-28T23:29:31',
-            modifiedDate: '2023-04-28T23:29:31',
-          },
-        ],
-        page: 1,
-        totalSurveys: 6,
-        totalPages: 5,
-      };
-      setSurveys(request.surveys);
-      setTotalPages(request.totalPages);
+      setSurveys(request.data.surveys);
+      setTotalPages(request.data.totalPages);
       setIsLoading(false);
     } catch (error) {
       const { name } = error as unknown as AxiosError;
