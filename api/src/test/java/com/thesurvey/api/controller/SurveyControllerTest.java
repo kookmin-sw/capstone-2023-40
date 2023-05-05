9fd69a46b42804008807394945b3bba9aa7cf50c
JinMyeong-Kim-Kr <kjmdkdlel@gmail.com>
2023-05-05 15:27:16 +0900

    fix: revise to incorrect usage of mapper method when getting a specific survey

--- api/src/test/java/com/thesurvey/api/controller/SurveyControllerTest.java
+++ api/src/test/java/com/thesurvey/api/controller/SurveyControllerTest.java
@@ -185,6 +185,7 @@ public class SurveyControllerTest extends BaseControllerTest {
             .isEqualTo(content.getString("endedDate"));
         assertThat(LocalDateTime.parse(content.getString("startedDate"))).isBefore(
             LocalDateTime.parse(content.getString("endedDate")));
+        assertThat(content.getJSONArray("questions")).isNotNull();
     }
 
     @Test
