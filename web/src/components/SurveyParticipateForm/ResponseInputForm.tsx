d8fff001bd5bf41f5b29a044ac05e00143be7d7c
sanghyun1128 <sanghyun1128@gmail.com>
2023-05-09 07:40:42 +0900

    refactor: 질문 종류에 따라 적절한 폼 보여주는 로직 분리

---
+++ web/src/components/SurveyParticipateForm/ResponseInputForm.tsx
@@ -0,0 +1,167 @@
+import React from 'react';
+
+import styled, { DefaultTheme } from 'styled-components';
+
+import { QuestionType } from '../../types/request';
+import { QuestionOptionResponse } from '../../types/response/QuestionOption';
+
+const MultipleChoiceContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
+`;
+
+// FIXME: radio button is working but need refactoring
+const ChoiceLabel = styled.label`
+  display: inline-flex;
+  align-items: center;
+  cursor: pointer;
+  font-size: 16px;
+  padding: 1.2vh 1vw 1.2vh 1vw;
+  margin-left: 1.3vw;
+  margin-bottom: 10px;
+  border-radius: ${(props) => props.theme.borderRadius};
+  border: ${(props) => props.theme.border};
+  width: 72vw;
+  color: ${(props) => props.theme.colors.default};
+  background-color: ${(props) => props.theme.colors.container};
+  transition: 200ms background ease;
+
+  &:hover {
+    background-color: ${(props) => props.theme.colors.background};
+  }
+`;
+
+const Input = styled.input`
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+
+  &:checked + span:before {
+    background: ${(props) => props.theme.colors.primary};
+    border: 7px solid ${(props) => props.theme.colors.primary};
+  }
+`;
+
+const Radio = styled(Input).attrs({ type: 'radio' })``;
+
+const CheckBox = styled(Input).attrs({ type: 'checkbox' })``;
+
+const Checkmark = styled.span`
+  position: relative;
+  display: inline-block;
+  height: 20px;
+  width: 20px;
+  margin-right: 15px;
+  background: transparent;
+  border: 2px solid #999da0;
+  border-radius: 50%;
+
+  &:before {
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    height: 10px;
+    width: 10px;
+    border-radius: 50%;
+  }
+`;
+
+const TextInput = styled.textarea`
+  padding: 1.2vh 2vw 1.2vh 2vw;
+  margin-bottom: 5px;
+  border-radius: ${(props) => props.theme.borderRadius};
+  border: ${(props) => props.theme.border};
+  resize: none;
+  font-weight: 100;
+  color: ${(props) => props.theme.colors.default};
+  background-color: ${(props) => props.theme.colors.container};
+`;
+
+const LongAnswer = styled(TextInput)`
+  width: 70%;
+  height: 70px;
+  font-size: 15px;
+`;
+
+const ShortAnswer = styled(TextInput)`
+  text-align: center;
+  width: 20vw;
+  height: 18px;
+  font-size: 15px;
+`;
+
+interface ResponseInputFormProps {
+  questionType: QuestionType;
+  options: Array<QuestionOptionResponse> | null;
+  questionBankId: number;
+  handleInputChange: (arg: React.ChangeEvent<HTMLTextAreaElement> | React.ChangeEvent<HTMLInputElement>) => void;
+  theme: DefaultTheme;
+}
+export default function ResponseInputForm({
+  questionType,
+  options,
+  questionBankId,
+  handleInputChange,
+  theme,
+}: ResponseInputFormProps) {
+  if (questionType === QuestionType.SINGLE_CHOICE || questionType === QuestionType.MULTIPLE_CHOICE) {
+    return (
+      <MultipleChoiceContainer theme={theme}>
+        {options?.map((option: QuestionOptionResponse) => (
+          <ChoiceLabel theme={theme} key={option.questionOptionId}>
+            {questionType === QuestionType.SINGLE_CHOICE ? (
+              <Radio
+                theme={theme}
+                name={String(questionBankId)}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  handleInputChange(event);
+                }}
+                value={`${option.questionOptionId}`}
+              />
+            ) : (
+              <CheckBox
+                theme={theme}
+                name={String(questionBankId)}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  handleInputChange(event);
+                }}
+                value={`${option.questionOptionId}`}
+              />
+            )}
+            <Checkmark theme={theme} />
+            {option.option}
+          </ChoiceLabel>
+        ))}
+      </MultipleChoiceContainer>
+    );
+  }
+
+  switch (questionType) {
+    case QuestionType.SHORT_ANSWER:
+      return (
+        <ShortAnswer
+          onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => {
+            handleInputChange(event);
+          }}
+          maxLength={19}
+          placeholder="답변을 입력해주세요"
+          theme={theme}
+        />
+      );
+    default:
+      return (
+        <LongAnswer
+          onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => {
+            handleInputChange(event);
+          }}
+          maxLength={450}
+          placeholder="답변을 입력해주세요"
+          theme={theme}
+        />
+      );
+  }
+}
--- web/src/components/SurveyParticipateForm/SurveyParticipateForm.tsx
+++ web/src/components/SurveyParticipateForm/SurveyParticipateForm.tsx
@@ -2,16 +2,24 @@ import React, { useEffect, useMemo, useRef, useState } from 'react';
 
 import styled, { DefaultTheme } from 'styled-components';
 
-import { QuestionType } from '../../types/request';
 import { QuestionBankResponse } from '../../types/response/QuestionBank';
-import { QuestionOptionResponse } from '../../types/response/QuestionOption';
 import { SurveyResponse } from '../../types/response/Survey';
 import { dateFormatUpToDate, getDDay } from '../../utils/dateFormat';
 import { scrollToRef } from '../../utils/scroll';
 import RectangleButton from '../Button/RectangleButton';
 import { SurveyPageResultModal } from '../Modal';
+import QuestionForm from './QuestionForm';
 
 const Container = styled.div``;
+
+const QuestionContainer = styled.div`
+  margin-top: 23px;
+  border-radius: ${(props) => props.theme.borderRadius};
+  border-left: 16px solid ${(props) => props.theme.colors.primary};
+  padding: 1.2vh 2vw 1.2vh 2vw;
+  color: ${(props) => props.theme.colors.default};
+  background-color: ${(props) => props.theme.colors.background};
+`;
 
 const HeadContainer = styled.div`
   padding: 6vh 8vw 0vh 8vw;
@@ -43,125 +51,21 @@ const BodyContainer = styled.div`
   background-color: ${(props) => props.theme.colors.container};
 `;
 
-const QuestionContainer = styled.div`
-  margin-top: 23px;
-  border-radius: ${(props) => props.theme.borderRadius};
-  border-left: 16px solid ${(props) => props.theme.colors.primary};
-  padding: 1.2vh 2vw 1.2vh 2vw;
-  color: ${(props) => props.theme.colors.default};
-  background-color: ${(props) => props.theme.colors.background};
-`;
-
-const QuestionTitle = styled.div`
-  width: 76vw;
-  padding: 1.2vh 1.5vw 1.2vh 1.5vw;
-  margin-top: 7px;
-  margin-bottom: 25px;
-  border: ${(props) => props.theme.border};
-  border-radius: ${(props) => props.theme.borderRadius};
-  font-size: 18px;
-  font-weight: 900;
-  color: ${(props) => props.theme.colors.default};
-  background-color: ${(props) => props.theme.colors.container};
-`;
-
-const Answer = styled.textarea`
-  padding: 1.2vh 2vw 1.2vh 2vw;
-  margin-bottom: 5px;
-  border-radius: ${(props) => props.theme.borderRadius};
-  border: ${(props) => props.theme.border};
-  resize: none;
-  font-weight: 100;
-  color: ${(props) => props.theme.colors.default};
-  background-color: ${(props) => props.theme.colors.container};
-`;
-
-const LongAnswer = styled(Answer)`
-  width: 75vw;
-  height: 70px;
-  font-size: 15px;
-`;
-
-const ShortAnswer = styled(Answer)`
-  text-align: center;
-  width: 20vw;
-  height: 18px;
-  font-size: 15px;
-  margin-left: 55vw;
-`;
-
-const MultipleChoiceContainer = styled.div`
-  padding: 0px;
-`;
-// FIXME: radio button is working but need refactoring
-const RadioContainer = styled.label`
-  display: inline-flex;
-  align-items: center;
-  cursor: pointer;
-  font-size: 16px;
-  padding: 1.2vh 1vw 1.2vh 1vw;
-  margin-left: 1.3vw;
-  margin-bottom: 10px;
-  border-radius: ${(props) => props.theme.borderRadius};
-  border: ${(props) => props.theme.border};
-  width: 75.7vw;
-  color: ${(props) => props.theme.colors.default};
-  background-color: ${(props) => props.theme.colors.container};
-
-  &:hover {
-    opacity: 0.8;
-  }
-`;
-
-const RadioInput = styled.input.attrs({ type: 'radio' })`
-  position: absolute;
-  opacity: 0;
-  cursor: pointer;
-
-  &:checked + span:before {
-    background: ${(props) => props.theme.colors.primary};
-    border: 7px solid ${(props) => props.theme.colors.primary};
-  }
-`;
-
-const RadioCheckmark = styled.span`
-  position: relative;
-  display: inline-block;
-  height: 20px;
-  width: 20px;
-  margin-right: 15px;
-  background: transparent;
-  border: 2px solid #999da0;
-  border-radius: 50%;
-
-  &:before {
-    content: '';
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    height: 10px;
-    width: 10px;
-    border-radius: 50%;
-  }
-`;
-
 interface SurveyParticipateFormProps {
   surveyData: SurveyResponse;
   theme: DefaultTheme;
 }
 
 export default function SurveyParticipateForm({ surveyData, theme }: SurveyParticipateFormProps) {
-  const nowDate = new Date();
   const questionRefs = useRef<HTMLDivElement[]>([]);
   const [endedDate, setEndedDate] = useState<string>('');
-  const [userAnswers, setUserAnswers] = useState<string[]>([]);
+  const [userResponses, setUserResponses] = useState<string[]>([]);
   const [resultModalOpen, setResultModalOpen] = useState<boolean>(false);
 
   const remainDate = useMemo(() => getDDay(endedDate), [endedDate]);
 
   const postSurveyAnswers = async () => {
-    console.log(userAnswers);
+    console.log(userResponses);
     // FIXME: post answers to server
     // axios.post(requests.postSurvey)
 
@@ -185,7 +89,7 @@ export default function SurveyParticipateForm({ surveyData, theme }: SurveyParti
     if (typeof surveyData !== 'undefined') {
       for (let i = 0; i < surveyData.questions.length; i += 1) {
         // TODO: response validation will be needed
-        if (typeof userAnswers[i] === 'undefined' || userAnswers[i] === '') {
+        if (typeof userResponses[i] === 'undefined' || userResponses[i] === '') {
           turnOnUserAttention(i);
           answersVerification = false;
           break;
@@ -205,42 +109,6 @@ export default function SurveyParticipateForm({ surveyData, theme }: SurveyParti
     }
   };
 
-  // FIXME: it cause rerendering every single typing
-  const handleAnswerChange = (
-    index: number,
-    event: React.ChangeEvent<HTMLTextAreaElement> | React.ChangeEvent<HTMLInputElement>
-  ) => {
-    const newInputs = [...userAnswers];
-    newInputs[index] = event.target.value;
-    setUserAnswers(newInputs);
-  };
-
-  // TODO: show current and max text count
-  const makeTextArea = (question: QuestionBankResponse, index: number) => {
-    if (question.questionType === QuestionType.LONG_ANSWER) {
-      return (
-        <LongAnswer
-          onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => {
-            handleAnswerChange(index, event);
-          }}
-          maxLength={450}
-          placeholder="답변을 입력해주세요"
-          theme={theme}
-        />
-      );
-    }
-    return (
-      <ShortAnswer
-        onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => {
-          handleAnswerChange(index, event);
-        }}
-        maxLength={19}
-        placeholder="답변을 입력해주세요"
-        theme={theme}
-      />
-    );
-  };
-
   useEffect(() => {
     if (typeof surveyData !== 'undefined') {
       setEndedDate(`${surveyData.endedDate}`);
@@ -257,7 +125,7 @@ export default function SurveyParticipateForm({ surveyData, theme }: SurveyParti
       </HeadContainer>
 
       <BodyContainer theme={theme}>
-        {surveyData?.questions?.map((question: QuestionBankResponse, index) => (
+        {surveyData?.questions?.map((question: QuestionBankResponse, index: number) => (
           <QuestionContainer
             theme={theme}
             key={question.questionBankId}
@@ -265,32 +133,17 @@ export default function SurveyParticipateForm({ surveyData, theme }: SurveyParti
               questionRefs.current[index] = element as HTMLDivElement;
             }}
           >
-            <QuestionTitle theme={theme}>
-              {index + 1}.&nbsp;&nbsp;{question.title}
-            </QuestionTitle>
-            {question.questionType === QuestionType.MULTIPLE_CHOICE ||
-            question.questionType === QuestionType.SINGLE_CHOICE ? (
-              <MultipleChoiceContainer theme={theme}>
-                {question.questionOptions.map((option: QuestionOptionResponse) => (
-                  <RadioContainer theme={theme} key={option.questionOptionId}>
-                    <RadioInput
-                      theme={theme}
-                      name={question.title}
-                      onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                        handleAnswerChange(index, event);
-                      }}
-                      value={`${option.questionOptionId}`}
-                    />
-                    <RadioCheckmark theme={theme} />
-                    {option.option}
-                  </RadioContainer>
-                ))}
-              </MultipleChoiceContainer>
-            ) : (
-              makeTextArea(question, index)
-            )}
+            <QuestionForm
+              key={question.questionBankId}
+              question={question}
+              index={index}
+              userResponses={userResponses}
+              setUserResponses={setUserResponses}
+              theme={theme}
+            />
           </QuestionContainer>
         ))}
+
         <ButtonContainer>
           <RectangleButton
             backgroundColor={theme.colors.primary}
