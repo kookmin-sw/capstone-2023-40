b96fe258b7e14718bcc5bc09ccdf8ff46f240c58
JinMyeong-Kim-Kr <kjmdkdlel@gmail.com>
2023-05-09 16:07:18 +0900

    refactor: remove MOBILE_PHONE and add IDENTITY_CARD certification

--- api/src/main/java/com/thesurvey/api/domain/EnumTypeEntity.java
+++ api/src/main/java/com/thesurvey/api/domain/EnumTypeEntity.java
@@ -1,5 +1,7 @@
 package com.thesurvey.api.domain;
 
+import lombok.Getter;
+
 public class EnumTypeEntity {
 
     public enum QuestionType {
@@ -9,19 +11,38 @@ public class EnumTypeEntity {
         LONG_ANSWER
     }
 
+    /**
+     * {@link CertificationType} represents various types of certifications
+     * that can be used for user certification.
+     * The available certification types are
+     * KAKAO, NAVER, GOOGLE, WEBMAIL, DRIVER_LICENSE, IDENTITY_CARD.
+     * {@code certificationTypeId} is the ID value that is saved in the database.
+     */
     public enum CertificationType {
-        KAKAO,
-        NAVER,
-        GOOGLE,
-        WEBMAIL,
-        DRIVER_LICENSE,
-        MOBILE_PHONE
+
+        KAKAO(0),
+
+        NAVER(1),
+
+        GOOGLE(2),
+
+        WEBMAIL(3),
+
+        DRIVER_LICENSE(4),
+
+        IDENTITY_CARD(5);
+
+        @Getter
+        private final int certificationTypeId;
+
+        CertificationType(int certificationTypeId) {
+            this.certificationTypeId = certificationTypeId;
+        }
     }
 
     public enum Role {
         USER,
         ADMIN
     }
-
 
 }
--- api/src/main/java/com/thesurvey/api/domain/ParticipationId.java
+++ api/src/main/java/com/thesurvey/api/domain/ParticipationId.java
@@ -21,7 +21,7 @@ public class ParticipationId implements Serializable {
     /*
      * the value of id is set to the index of EnumTypeEntity.CertificationType.
      * the value of id : CertificationType
-     * 0: KAKAO, 1: NAVER, 2: GOOGLE, 3: WEBMAIL, 4: DRIVER_LICENSE, 5: MOBILE_PHONE
+     * 0: KAKAO, 1: NAVER, 2: GOOGLE, 3: WEBMAIL, 4: DRIVER_LICENSE, 5: IDENTITY_CARD
      */
     @Column(name = "certification_type")
     private CertificationType certificationType;
