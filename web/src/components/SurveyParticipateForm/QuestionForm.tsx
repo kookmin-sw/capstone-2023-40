b84fb51723fad8418df52c572aab9a4aab79350c
sanghyun1128 <sanghyun1128@gmail.com>
2023-05-09 14:00:21 +0900

    feat: 다중선택 질문 작동로직

--- web/src/components/SurveyParticipateForm/QuestionForm.tsx
+++ web/src/components/SurveyParticipateForm/QuestionForm.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styled, { DefaultTheme } from 'styled-components';
 
+import { AnsweredQuestion, QuestionType } from '../../types/request';
 import { QuestionBankResponse } from '../../types/response/QuestionBank';
 import ResponseInputForm from './ResponseInputForm';
 
@@ -28,6 +29,7 @@ const TextLabel = styled.label`
   font-weight: 900;
   color: ${(props) => props.theme.colors.default};
 `;
+
 const Title = styled(TextLabel)`
   font-size: 18px;
   background-color: ${(props) => props.theme.colors.container};
@@ -42,17 +44,59 @@ const Description = styled(TextLabel)`
 interface QuestionFormProps {
   question: QuestionBankResponse;
   index: number;
-  userResponses: string[];
-  setUserResponses: (arg: string[]) => void;
+  userResponses: AnsweredQuestion[];
+  setUserResponses: (arg: AnsweredQuestion[]) => void;
   theme: DefaultTheme;
 }
+
 export default function QuestionForm({ question, index, userResponses, setUserResponses, theme }: QuestionFormProps) {
-  // FIXME: it cause rerendering every single typing
-  const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement> | React.ChangeEvent<HTMLInputElement>) => {
-    const newInputs = [...userResponses];
-    newInputs[index] = event.target.value;
-    setUserResponses(newInputs);
+  const [choiceIsChecked, setChoiceIsChecked] = useState<boolean>(false);
+
+  const editSelectedChoiceList = (value: number, isChecked: boolean) => {
+    const newUserResponses = [...userResponses];
+
+    if (typeof newUserResponses[index] === 'undefined') {
+      newUserResponses[index] = { questionBankId: question.questionBankId };
+    }
+
+    if (isChecked) {
+      newUserResponses[index] = {
+        questionBankId: question.questionBankId,
+        multipleChoices: [...(newUserResponses[index].multipleChoices || []), value],
+      };
+    } else {
+      newUserResponses[index] = {
+        questionBankId: question.questionBankId,
+        multipleChoices: newUserResponses[index].multipleChoices?.filter((item: number) => item !== value),
+      };
+    }
+
+    setUserResponses(newUserResponses);
   };
+
+  const handleChangeCheck = (event: React.ChangeEvent<HTMLInputElement>, value: number) => {
+    setChoiceIsChecked(!choiceIsChecked);
+    editSelectedChoiceList(value, event.target.checked);
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement> | React.ChangeEvent<HTMLInputElement>) => {
+    const newUserResponses = [...userResponses];
+    switch (question.questionType) {
+      case QuestionType.SINGLE_CHOICE:
+        newUserResponses[index] = { questionBankId: question.questionBankId, singleChoice: Number(event.target.value) };
+        break;
+      case QuestionType.SHORT_ANSWER:
+        newUserResponses[index] = { questionBankId: question.questionBankId, shortAnswer: event.target.value };
+        break;
+      case QuestionType.LONG_ANSWER:
+        newUserResponses[index] = { questionBankId: question.questionBankId, longAnswer: event.target.value };
+        break;
+      default:
+        break;
+    }
+    setUserResponses(newUserResponses);
+  };
+
   // TODO: show current and max text count
   return (
     <Container>
@@ -69,6 +113,7 @@ export default function QuestionForm({ question, index, userResponses, setUserRe
           options={question.questionOptions}
           questionBankId={question.questionBankId}
           handleInputChange={handleInputChange}
+          handleChangeCheck={handleChangeCheck}
           theme={theme}
         />
       </BodyContainer>
