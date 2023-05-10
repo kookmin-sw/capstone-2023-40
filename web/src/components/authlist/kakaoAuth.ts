ebfe0ad334cd8dd8a14834f88cee0b5688516cee
오성혁 <68771488+ohsung1125@users.noreply.github.com>
2023-05-10 21:29:20 +0900

    feat: 카카오 인증 서비스 구현 완성 - 사용자 정보 조회

--- web/src/components/authlist/kakaoAuth.ts
+++ web/src/components/authlist/kakaoAuth.ts
@@ -1,15 +1,50 @@
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+
+import { setCompleteAuth } from '../../types/surveyAuth';
+
+export const KAKAO_REST_API_KEY = '076bd4745b2957a7567361ad04b58a57';
+export const KAKAO_REDIRECT_URI = 'http://localhost:3000/mypage/authentication';
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
 /**
  * Auth Service with Kakao API Connecting
  * develop's KAKAO URL : http://localhost:3000/mypage/authentication
  * users's KAKAO URL : http://thesurvey.kr/mypage/authentication
  */
-export default function authServiceWithKakao() {
-  const KAKAO_REST_API_KEY = '51feec3de9b441289713bc0d98fee11b';
-  const KAKAO_REDIRECT_URI = 'http://localhost:3000/mypage/authentication';
-  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
-
-  window.location.href = KAKAO_AUTH_URL;
-}
-
-// FIXME: turn on KAKAO_REDIRECT_URL in thesurvey Service
+// FIXME: turn on KAKAO_REDIRECT_URL in thesurvey Service,
+// FIXME: process.env로 KEY값 보안설정하기.
 // const KAKAO_REDIRECT_URI = 'http://thesurvey.kr/mypage/authentication';
+
+export const getKakaoProfile = async (kakaoToken: any, username: string, dispatch = useDispatch()) => {
+  try {
+    const kakaoUser = await axios({
+      method: 'GET',
+      url: `https://kapi.kakao.com/v2/user/me`,
+      headers: {
+        Authorization: `Bearer ${kakaoToken}`,
+      },
+    });
+    const name = kakaoUser.data.properties.nickname;
+    if (name === username) {
+      console.log('이름이 똑같아요!', name);
+      dispatch(setCompleteAuth(true));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getKakaoUserData = async (authCode: string, username: string, dispatch = useDispatch()) => {
+  const params = new URLSearchParams();
+  params.append('grant_type', 'authorization_code');
+  params.append('client_id', KAKAO_REST_API_KEY); // 카카오 디벨로퍼스에서 발급받은 클라이언트 아이디
+  params.append('redirect_uri', KAKAO_REDIRECT_URI); // 카카오 디벨로퍼스에서 설정한 리다이렉트 URI
+  params.append('code', authCode);
+
+  const response = await axios.post('https://kauth.kakao.com/oauth/token', params.toString(), {
+    headers: {
+      'Content-type': 'application/x-www-form-urlencoded;charset=utf-8',
+    },
+  });
+  getKakaoProfile(response.data.access_token, username, dispatch);
+};
