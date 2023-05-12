4c74f6db005af63242bc83c2cc87753305a6e496
오성혁 <68771488+ohsung1125@users.noreply.github.com>
2023-05-12 13:36:48 +0900

    feat: 카카오 인증 과정 설명

--- web/src/components/authlist/kakaoAuth.ts
+++ web/src/components/authlist/kakaoAuth.ts
@@ -3,18 +3,22 @@ import { useDispatch } from 'react-redux';
 
 import { setCompleteAuth, setSuccessAuth } from '../../types/surveyAuth';
 
-export const KAKAO_REST_API_KEY = '076bd4745b2957a7567361ad04b58a57';
-export const KAKAO_REDIRECT_URI = 'http://localhost:3000/mypage/authentication';
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
+// FIXME: turn on KAKAO_REDIRECT_URL in thesurvey Service,
 /**
  * Auth Service with Kakao API Connecting
+ * @KAKAO_REST_API_KEY : kakao developers에서 발급받은 Client ID
+ * @KAKAO_REDIRECT_URL : kakao developers에서 설정한 사용가능 한 URI
  * develop's KAKAO URL : http://localhost:3000/mypage/authentication
  * users's KAKAO URL : http://thesurvey.kr/mypage/authentication
  */
-// FIXME: turn on KAKAO_REDIRECT_URL in thesurvey Service,
-// FIXME: process.env로 KEY값 보안설정하기.
-// const KAKAO_REDIRECT_URI = 'http://thesurvey.kr/mypage/authentication';
+export const KAKAO_REST_API_KEY = '076bd4745b2957a7567361ad04b58a57';
+export const KAKAO_REDIRECT_URI = 'http://localhost:3000/mypage/authentication';
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
 
+/**
+ * 카카오 사용자 정보 조회를 위한 Function
+ * @param kakaoToken : 카카오에서 받은 인자코드를 사용하여 받은 token값
+ */
 export const getKakaoProfile = async (kakaoToken: any, username: string, dispatch = useDispatch()) => {
   try {
     const kakaoUser = await axios({
@@ -38,11 +42,17 @@ export const getKakaoProfile = async (kakaoToken: any, username: string, dispatc
   }
 };
 
+/**
+ * 카카오에서 받는 인자 코드로 사용자 정보 조회하기. (인자 코드 -> token -> user data)
+ * @param authCode : 카카오에서 로그인 후 받은 고유 인자코드(로그인 성공시 받음)
+ * @param username : The Survey에서 사용하는 사용자의 이름.
+ * @param dispatch : 페이지변경이 일어나도 인증과정에 필요한 변수값들을 유지.
+ */
 export const getKakaoUserData = async (authCode: string, username: string, dispatch = useDispatch()) => {
   const params = new URLSearchParams();
   params.append('grant_type', 'authorization_code');
-  params.append('client_id', KAKAO_REST_API_KEY); // 카카오 디벨로퍼스에서 발급받은 클라이언트 아이디
-  params.append('redirect_uri', KAKAO_REDIRECT_URI); // 카카오 디벨로퍼스에서 설정한 리다이렉트 URI
+  params.append('client_id', KAKAO_REST_API_KEY);
+  params.append('redirect_uri', KAKAO_REDIRECT_URI);
   params.append('code', authCode);
 
   const response = await axios.post('https://kauth.kakao.com/oauth/token', params.toString(), {
