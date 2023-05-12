790236e64f4a8baf2c41c918be7209e435beadcb
sanghyun1128 <sanghyun1128@gmail.com>
2023-05-12 16:24:26 +0900

    refactor: 주석 수정

--- web/src/utils/dateFormat.ts
+++ web/src/utils/dateFormat.ts
@@ -1,7 +1,7 @@
 /**
- * Replace T from Date format(yyyy-mm-ddThh:MM:ss)
+ * Replace 'T' from Date format(yyyy-mm-dd'T'hh:MM:ss)
  *
- * @param {string} date yyyy-mm-ddThh:MM:ss
+ * @param {string} date yyyy-mm-dd'T'hh:MM:ss
  * @returns {string} yyyy-mm-dd hh:MM
  */
 export const dateFormatUpToSecond = (date: string): string => {
@@ -9,7 +9,7 @@ export const dateFormatUpToSecond = (date: string): string => {
 };
 
 /**
- * Replace T and remove seconds from Date format(yyyy-mm-ddThh:MM:ss)
+ * Replace 'T' and remove seconds from Date format(yyyy-mm-dd'T'hh:MM:ss)
  *
  * @param {string} date yyyy-mm-ddThh:MM:ss
  * @returns {string} yyyy-mm-dd hh:MM
@@ -19,9 +19,9 @@ export const dateFormatUpToMinute = (date: string): string => {
 };
 
 /**
- * Replace T and remove hour, minute and seconds from Date format(yyyy-mm-ddThh:MM:ss)
+ * Replace 'T' and remove hour, minute and seconds from Date format(yyyy-mm-ddThh:MM:ss)
  *
- * @param {string} date yyyy-mm-ddThh:MM:ss
+ * @param {string} date yyyy-mm-dd'T'hh:MM:ss
  * @returns {string} yyyy-mm-dd
  */
 export const dateFormatUpToDate = (date: string): string => {
@@ -31,8 +31,8 @@ export const dateFormatUpToDate = (date: string): string => {
 /**
  * Return how many days are left before the dDate from now
  *
- * @param {string | Date} dDate yyyy-mm-ddThh:MM:ss
- * @returns {number} difference date
+ * @param {string | Date} dDate yyyy-mm-dd'T'hh:MM:ss
+ * @returns {number} date difference
  */
 export const getDDay = (dDate: string | Date): number => {
   const dateDiff = new Date(dDate).getTime() - new Date().getTime();
