c5c4e45e8c561a7918f51f6d9e9cc1df26270888
JinMyeong-Kim-Kr <kjmdkdlel@gmail.com>
2023-05-16 23:25:04 +0900

    refactor: integrate CertificationInfo class

--- api/src/main/java/com/thesurvey/api/dto/response/userCertification/CertificationInfo.java
+++ api/src/main/java/com/thesurvey/api/dto/response/userCertification/CertificationInfo.java
@@ -2,7 +2,6 @@ package com.thesurvey.api.dto.response.userCertification;
 
 import java.time.LocalDateTime;
 
-
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
@@ -10,13 +9,15 @@ import lombok.Getter;
 
 @Getter
 @Builder
-public class KakaoCertificationInfoDto {
+public class CertificationInfo<CertificationType> {
 
-    @Schema(example = "true", description = "카카오 인증 여부입니다.")
+    @Schema(example = "KAKAO", description = "인증 정보 이름입니다.")
+    private String certificationName;
+
+    @Schema(example = "false", description = "인증 여부입니다.")
     private Boolean isCertificated;
 
-    @Schema(example = "2023-04-22T00:00:00", description = "카카오 인증 만료 날짜입니다.")
+    @Schema(example = "2023-04-22T00:00:00", description = "인증 만료 날짜입니다.")
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime expirationDate;
-
 }
--- api/src/main/java/com/thesurvey/api/service/mapper/UserCertificationMapper.java
+++ api/src/main/java/com/thesurvey/api/service/mapper/UserCertificationMapper.java
@@ -1,17 +1,13 @@
 package com.thesurvey.api.service.mapper;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.thesurvey.api.domain.EnumTypeEntity.CertificationType;
 import com.thesurvey.api.domain.UserCertification;
-import com.thesurvey.api.dto.response.userCertification.DriverLicenseCertificationInfoDto;
-import com.thesurvey.api.dto.response.userCertification.GoogleCertificationInfoDto;
-import com.thesurvey.api.dto.response.userCertification.IdentityCardCertificationInfoDto;
-import com.thesurvey.api.dto.response.userCertification.KakaoCertificationInfoDto;
-import com.thesurvey.api.dto.response.userCertification.NaverCertificationInfoDto;
+import com.thesurvey.api.dto.response.userCertification.CertificationInfo;
 import com.thesurvey.api.dto.response.userCertification.UserCertificationListDto;
-import com.thesurvey.api.dto.response.userCertification.WebMailCertificationInfoDto;
 import com.thesurvey.api.repository.UserCertificationRepository;
 
 import org.springframework.stereotype.Component;
@@ -29,96 +25,21 @@ public class UserCertificationMapper {
         List<UserCertification> userCertificationList =
             userCertificationRepository.findUserCertificationByUserId(userId);
 
-        UserCertificationListDto userCertificationListDto = new UserCertificationListDto();
+        List<CertificationInfo<CertificationType>> certificationInfoList = new ArrayList<>();
         for (UserCertification userCertification : userCertificationList) {
             CertificationType certificationType = userCertification.getCertificationType();
-            setCertificationInfo(userCertificationListDto, userCertification, certificationType);
+            certificationInfoList.add(getCertificationInfo(certificationType, userCertification.getExpirationDate()));
         }
-        return userCertificationListDto;
-    }
-
-    public void setCertificationInfo(UserCertificationListDto userCertificationListDto,
-        UserCertification userCertification,
-        CertificationType certificationType) {
-        LocalDateTime expirationDate = userCertification.getExpirationDate();
-        switch (certificationType) {
-            case KAKAO:
-                userCertificationListDto.setKakaoCertificationInfo(
-                    buildKakaoCertificationInfoDto(expirationDate)
-                );
-                break;
-
-            case NAVER:
-                userCertificationListDto.setNaverCertificationInfo(
-                    buildNaverCertificationInfoDto(expirationDate)
-                );
-                break;
-
-            case GOOGLE:
-                userCertificationListDto.setGoogleCertificationInfo(
-                    buildGoogleCertificationInfoDto(expirationDate)
-                );
-                break;
-
-            case WEBMAIL:
-                userCertificationListDto.setWebMailCertificationInfo(
-                    buildWebMailCertificationInfoDto(expirationDate)
-                );
-                break;
-
-            case DRIVER_LICENSE:
-                userCertificationListDto.setDriverLicenseCertificationInfo(
-                    buildDriverLicenseCertificationInfoDto(expirationDate)
-                );
-                break;
-
-            case IDENTITY_CARD:
-                userCertificationListDto.setIdentityCardCertificationInfo(
-                    buildIdentityCardCertificationInfoDto(expirationDate)
-                );
-                break;
-        }
-    }
-
-    private KakaoCertificationInfoDto buildKakaoCertificationInfoDto(LocalDateTime expirationDate) {
-        return KakaoCertificationInfoDto.builder()
-            .isCertificated(true)
-            .expirationDate(expirationDate)
+        return UserCertificationListDto.builder()
+            .certificationInfoList(certificationInfoList)
             .build();
     }
 
-    private NaverCertificationInfoDto buildNaverCertificationInfoDto(LocalDateTime expirationDate) {
-        return NaverCertificationInfoDto.builder()
-            .isCertificated(true)
+    private CertificationInfo<CertificationType> getCertificationInfo(CertificationType certificationType, LocalDateTime expirationDate) {
+        return CertificationInfo.<CertificationType>builder()
+            .certificationName(certificationType.name())
             .expirationDate(expirationDate)
-            .build();
-    }
-
-    private GoogleCertificationInfoDto buildGoogleCertificationInfoDto(LocalDateTime expirationDate) {
-        return GoogleCertificationInfoDto.builder()
             .isCertificated(true)
-            .expirationDate(expirationDate)
-            .build();
-    }
-
-    private WebMailCertificationInfoDto buildWebMailCertificationInfoDto(LocalDateTime expirationDate) {
-        return WebMailCertificationInfoDto.builder()
-            .isCertificated(true)
-            .expirationDate(expirationDate)
-            .build();
-    }
-
-    private DriverLicenseCertificationInfoDto buildDriverLicenseCertificationInfoDto(LocalDateTime expirationDate) {
-        return DriverLicenseCertificationInfoDto.builder()
-            .isCertificated(true)
-            .expirationDate(expirationDate)
-            .build();
-    }
-
-    private IdentityCardCertificationInfoDto buildIdentityCardCertificationInfoDto(LocalDateTime expirationDate) {
-        return IdentityCardCertificationInfoDto.builder()
-            .isCertificated(true)
-            .expirationDate(expirationDate)
             .build();
     }
 
