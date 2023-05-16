ee537f12517fa8ad02142f39d1ffb0a48509ab1b
JinMyeong-Kim-Kr <kjmdkdlel@gmail.com>
2023-05-16 23:25:04 +0900

    refactor: integrate CertificationInfo class

--- api/src/main/java/com/thesurvey/api/dto/response/userCertification/UserCertificationListDto.java
+++ api/src/main/java/com/thesurvey/api/dto/response/userCertification/UserCertificationListDto.java
@@ -1,31 +1,17 @@
 package com.thesurvey.api.dto.response.userCertification;
 
+import java.util.List;
+
+import com.thesurvey.api.domain.EnumTypeEntity.CertificationType;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
-@NoArgsConstructor
+@Builder
 public class UserCertificationListDto {
 
-    @Schema(description = "카카오 인증 정보입니다.")
-    private KakaoCertificationInfoDto kakaoCertificationInfo;
-
-    @Schema(description = "네이버 인증 정보입니다.")
-    private NaverCertificationInfoDto naverCertificationInfo;
-
-    @Schema(description = "구글 인증 정보입니다.")
-    private GoogleCertificationInfoDto googleCertificationInfo;
-
-    @Schema(description = "웹메일 인증 정보입니다.")
-    private WebMailCertificationInfoDto webMailCertificationInfo;
-
-    @Schema(description = "운전면허증 인증 정보입니다.")
-    private DriverLicenseCertificationInfoDto driverLicenseCertificationInfo;
-
-    @Schema(description = "신분증 인증 정보입니다.")
-    private IdentityCardCertificationInfoDto identityCardCertificationInfo;
+    @Schema(description = "인증 정보 목록입니다.")
+    private List<CertificationInfo<CertificationType>> certificationInfoList;
 
 }
