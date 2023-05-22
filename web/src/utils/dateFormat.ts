48d1359faccd0c0d1bde4ceeb7c1993ff69f0ec9
SeiwonPark <psw7347@gmail.com>
2023-05-22 22:48:37 +0900

    feat: add timer of the survey to be started

--- web/src/utils/dateFormat.ts
+++ web/src/utils/dateFormat.ts
@@ -39,3 +39,23 @@ export const getDDay = (dDate: string | Date): number => {
 
   return Math.floor(Math.abs(dateDiff / (1000 * 60 * 60 * 24)));
 };
+
+/**
+ * Returns remaining time to given `date` from now.
+ *
+ * @param {string | Date} date yyyy-mm-dd'T'hh:MM:ss
+ * @returns Remaining days, hours, minutes, seconds in number type each.
+ */
+export const getTimeRemaining = (date: string) => {
+  const startDate = new Date(date);
+  const now = new Date();
+
+  const totalSeconds = Math.floor((startDate.getTime() - now.getTime()) / 1000);
+
+  const days = Math.floor(totalSeconds / 86400);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = totalSeconds % 60;
+
+  return { days, hours, minutes, seconds };
+};
