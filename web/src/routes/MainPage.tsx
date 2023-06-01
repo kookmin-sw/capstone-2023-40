338a7b32103c55ab2a2733dd51abe0d1fbbda641
JinMyeong-Kim-Kr <kjmdkdlel@gmail.com>
2023-06-01 17:28:25 +0900

    feat: add animation and second container

--- web/src/routes/MainPage.tsx
+++ web/src/routes/MainPage.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+import { isMobile } from 'react-device-detect';
 
 import BackgroundImage from '../assets/main-page.webp';
 import Header from '../components/Header';
@@ -20,7 +21,7 @@ const Container = styled.div`
 `;
 
 const Introduction = styled.div`
-  margin-top: 30vh;
+  padding-top: 25vh;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -30,9 +31,21 @@ const Introduction = styled.div`
   font-weight: 500;
 `;
 
+const MobileIntroduction = styled.div`
+  padding-top: 20vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  color: white;
+  font-size: 3vh;
+  font-weight: 500;
+`;
+
 const Description = styled.p`
   word-break: break-word;
   word-break: keep-all;
+  line-height: 8vh;
 `;
 
 const AppTitle = styled.span`
@@ -57,25 +70,86 @@ const Button = styled.button`
   }
 `;
 
-export default function MainPage() {
-  const [theme, toggleTheme] = useTheme();
-  const navigate = useNavigate();
-  const isLoggedIn = useSelector((state: RootState) => state.header.isLoggedIn);
+const FadeInUp = keyframes`
+  0% {
+    opacity: 0;
+    transform: translate3d(0, 100%, 0);
+  }
+  to {
+    opacity: 1;
+    transform: translateZ(0);
+  }
+`;
 
-  return (
-    <Container>
-      <Header theme={theme} toggleTheme={toggleTheme} />
-      <Introduction>
-        <Description>
-          설문의 모든 것
-          <br />
-          <AppTitle>더 서베이</AppTitle>
-          에서 쉽고 간편하게
-        </Description>
-        <Button onClick={() => navigate(isLoggedIn ? '/survey' : '/login')} theme={theme}>
-          바로 설문하기
-        </Button>
-      </Introduction>
-    </Container>
-  );
+const FadeInUpAnimationText = styled.span`
+  animation: ${FadeInUp} 2s ease-in-out;
+`;
+
+
+export default function MainPage() {
+    const [theme, toggleTheme] = useTheme();
+    const navigate = useNavigate();
+    const isLoggedIn = useSelector((state: RootState) => state.header.isLoggedIn);
+
+    return (
+        <div>
+            <Container>
+                <Header theme={theme} toggleTheme={toggleTheme}/>
+                {!isMobile ? (
+                    <Introduction>
+                        <Description>
+                            <FadeInUpAnimationText>
+                                설문의 모든 것
+                                <br/>
+                                <AppTitle>더 서베이</AppTitle>
+                                에서 쉽고 간편하게</FadeInUpAnimationText>
+                        </Description>
+                        <Button
+                            onClick={() => navigate(isLoggedIn ? '/survey' : '/login')}
+                            theme={theme}>
+                            바로 설문하기
+                        </Button>
+                    </Introduction>
+                ) : (
+                    <MobileIntroduction>
+                        <Description>
+                            <FadeInUpAnimationText>
+                                설문의 모든 것
+                                <br/>
+                                <AppTitle>더 서베이</AppTitle>
+                                에서 쉽고 간편하게</FadeInUpAnimationText>
+                        </Description>
+                        <Button
+                            onClick={() => navigate(isLoggedIn ? '/survey' : '/login')}
+                            theme={theme}>
+                            바로 설문하기
+                        </Button>
+                    </MobileIntroduction>
+                )}
+            </Container>
+            <Container>
+                {!isMobile ? (
+                    <Introduction>
+                        <Description>
+                            <AppTitle>더 서베이</AppTitle>에서는 웹, 모바일 어디서나 설문조사를 만들거나 참여할 수 있어요
+                            <br/>
+                            여러 인증을 통해서 보다 <AppTitle>믿을 수 있는</AppTitle> 설문조사를 만들 수 있어요
+                            <br/>
+                            설문조사를 참여해서 얻은 포인트로 <AppTitle>기프티콘</AppTitle>을 교환할 수 있어요
+                        </Description>
+                    </Introduction>) : (
+                    <MobileIntroduction>
+                        <Description>
+                            <AppTitle>더 서베이</AppTitle>에서는 웹, 모바일 어디서나 설문조사를 만들거나 참여할 수 있어요
+                            <br/>
+                            여러 인증을 통해서 보다 <AppTitle>믿을 수 있는</AppTitle> 설문조사를 만들 수 있어요
+                            <br/>
+                            설문조사를 참여해서 얻은 포인트로 <AppTitle>기프티콘</AppTitle>을 교환할 수 있어요
+                        </Description>
+                    </MobileIntroduction>
+                    )
+                }
+            </Container>
+        </div>
+    );
 }
